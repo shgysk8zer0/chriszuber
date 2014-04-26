fecd3691d04656b8a42587640aa8117949116454
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-04-26 16:12:10 -0700

    Added an example for watch() to body

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,15 +1,31 @@
-'use strict';
-var html = document.documentElement,
-	body = document.body,
-	head = document.head;
 window.addEventListener('load',function(){
-	html.classList.remove('no-js');
-	['querySelectorAll', 'svg', 'audio', 'video', 'canvas', 'dataset', 'connectivity', 'visibility', 'notifications', 'ApplicationCache', 'indexedDB', 'transitions', 'animations', 'cssSupports', 'cssVars', 'workers', 'promises', 'ajax'].forEach(function(sup) {
-		(supports(sup)) ? html.classList.add(sup) : html.classList.add('no-' + sup);
-	});
+	var html = $(document.documentElement),
+		body = $(document.body),
+		head = $(document.head);
+	html.removeClass('no-js');
+	for(let feature of ['querySelectorAll', 'svg', 'audio', 'video', 'canvas', 'dataset', 'connectivity', 'visibility', 'notifications', 'ApplicationCache', 'indexedDB', 'transitions', 'animations', 'cssSupports', 'cssVars', 'workers', 'promises', 'ajax']){
+		(supports(feature)) ? html.addClass(feature) : html.addClass('no-' + sup);
+	};
+	(!document.documentElement.classList.contains('connectivity') && !navigator.onLine) ? html.addClass('offline')  : html.addClass('online');
+	(!document.documentElement.classList.contains('visibility') && document.hidden) ? html.addClass('hidden')  : html.addClass('visible');
 	$('a.confirm[href]').click(function(event){
 		if(!confirm('Go to ' + this.href + '?')) {
 			event.preventDefault();
 		}
 	});
+	body.watch({
+		childList: function(){
+			console.log(this.addedNodes)
+		},
+		attributes: function(){
+			console.log(this.attributeName, this.oldValue)
+		},
+		characterData: function(){
+			console.log(this.oldValue)
+		}
+	}, [
+		'subtree',
+		'attributeOldValue',
+		'characterDataOldValue'
+	]);
 });
