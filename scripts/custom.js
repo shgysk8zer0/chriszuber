424b7d7456f984574a5eb3a252897b3e3a8d0609
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-04-26 09:01:32 -0700

    New feature detection and option to confirm navigation when clicking <a>

--- scripts/custom.js
+++ scripts/custom.js
@@ -4,7 +4,12 @@ var html = document.documentElement,
 	head = document.head;
 window.addEventListener('load',function(){
 	html.classList.remove('no-js');
-	['transitions'].forEach(function(sup) {
+	['querySelectorAll', 'svg', 'audio', 'video', 'canvas', 'dataset', 'connectivity', 'visibility', 'notifications', 'ApplicationCache', 'indexedDB', 'transitions', 'animations', 'cssSupports', 'cssVars', 'workers', 'promises', 'ajax'].forEach(function(sup) {
 		(supports(sup)) ? html.classList.add(sup) : html.classList.add('no-' + sup);
-	})
+	});
+	$('a.confirm[href]').click(function(event){
+		if(!confirm('Go to ' + this.href)) {
+			event.preventDefault();
+		}
+	});
 });
