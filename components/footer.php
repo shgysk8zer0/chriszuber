4a602e5ebd8df9939464d96e6b5939ec979b92cb
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-04-29 18:33:12 -0700

    Removed debug($storage)

--- components/footer.php
+++ components/footer.php
@@ -4,5 +4,4 @@
 		<a href="https://github.com/shgysk8zer0/chriszuber" target="_blank" class="logo confirm">
 			<?php require('images/logos/github.svg')?>
 		</a>
-		<?php debug($storage)?>
 	</footer>
