ccd161c2aa36fbb5a42f991daedbeeb060468b17
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-04-30 17:16:42 -0700

    Add title to header>h1 and button to load contact_card

--- components/header.php
+++ components/header.php
@@ -1,4 +1,8 @@
+<?php
+	$session = session::load();
+	$storage = storage::load();
+?>
 	<header>
-		<h1></h1>
-		<nav></nav>
+		<h1><?=$storage->site_info->title?></h1>
+		<nav><button type="button" data-request="load=contact_card" data-icon="U"></button></nav>
 	</header>
