12a03176688043ce972d1398424898e76cc952ac
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-05-02 17:26:10 -0700

    Added Observer to body and subtree, watching for new Nodes and attribute changes.
    New Nodes triggers bootstrap (listeners and such),
    and attributes triggers changing of menus.

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,9 +1,10 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', function(){ /*Cannot rely on $(window).load() to work, so use this instead*/
+	'use strict';
 	var html = $(document.documentElement),
 		body = $(document.body),
 		head = $(document.head);
-	html.removeClass('no-js');
-	['svg', 'audio', 'video', 'canvas', 'dataset', 'connectivity', 'visibility', 'notifications', 'ApplicationCache', 'indexedDB', 'transitions', 'animations',  'CSSvars', 'CSSsupports', 'CSSmatches', 'querySelectorAll', 'workers', 'promises', 'ajax', 'FormData'].forEach(function(support){
+	html.removeClass('no-js').addClass('js');
+	['svg', 'audio', 'video', 'canvas', 'menuitem', 'dataset', 'connectivity', 'visibility', 'notifications', 'ApplicationCache', 'indexedDB', 'transitions', 'animations',  'CSSvars', 'CSSsupports', 'CSSmatches', 'querySelectorAll', 'workers', 'promises', 'ajax', 'FormData'].forEach(function(support){
 		(supports(support)) ? html.addClass(support) : html.addClass('no-' + support);
 	});
 	document.body.bootstrap();
@@ -11,8 +12,29 @@ window.addEventListener('load', function () {
 		childList: function(){
 			this.addedNodes.bootstrap();
 		},
+		attributes: function(){
+			switch(this.attributeName) {
+				case 'data-menu':
+					var menu = this.target.data('menu');
+					if(menu && menu !== '') {
+						this.target.setAttribute('contextmenu', menu + '_menu');
+						if($('menu#'+ menu + '_menu').length === 0){
+							ajax({
+								url: document.baseURI,
+								request: 'load_menu=' + menu
+							}).then(handleXHRjson, console.error);
+						}
+						this.target.removeAttribute('data-menu');
+					}
+					break;
+				case 'contextmenu':
+					(this.oldValue !== '') && $('menu#' + this.oldValue).delete();
+					break;
+			}
+		}
 	}, [
-		'subtree'
+		'subtree',
+		'attributeOldValue'
 	]);
 	$(window).online(function(){
 		$('fieldset').each(function(fieldset){
@@ -34,3 +56,13 @@ window.addEventListener('load', function () {
 		});
 	});
 });
+Element.prototype.worker_clock=function(){
+	var clock=document.createElement('time'),
+	clockWorker=new Worker(document.baseURI + 'scripts/workers/clock.js');
+	this.appendChild(clock);
+	clockWorker.addEventListener('message',function(e){
+		clock.innerHTML=e.data.norm;
+		clock.setAttribute('datetime',e.data.datetime);
+	});
+	clockWorker.postMessage('');
+}
