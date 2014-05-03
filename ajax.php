2f979890e2b3f11eeff1cc8d1e074cdbd8284057
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-05-03 14:50:20 -0700

    Indent changes...

--- ajax.php
+++ ajax.php
@@ -1,23 +1,25 @@
 <?php
-if(array_key_exists('load', $_POST)){
-	switch($_POST['load']) {
-		default:
-			json_response([
-				'html' => [
-					'main' => load_results($_POST['load'])
-				]
-			]);
+	if(array_key_exists('load', $_POST)){
+		switch($_POST['load']) {
+			default:
+				json_response([
+					'html' => [
+						'main' => load_results($_POST['load'])
+					]
+				]);
+		}
 	}
-}
-elseif(array_key_exists('load_menu', $_POST)) {
-	switch($_POST['load_menu']) {
-		default:
-			json_response([
-				'prepend' => [
-					'body' => load_results("menus/{$_POST['load_menu']}")
-				]
-			]);
+	elseif(array_key_exists('load_menu', $_POST)) {
+		switch($_POST['load_menu']) {
+			default:
+				json_response([
+					'prepend' => [
+						'body' => load_results("menus/{$_POST['load_menu']}")
+					]
+				]);
+		}
 	}
-}
+
+	else http_status(404);
 	exit();
 ?>
