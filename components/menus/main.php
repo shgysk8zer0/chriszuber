9564662576e1075c29a06bea049d7815800e8a92
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-05-17 14:15:09 -0700

    Login/lougout enabled/disabled according to login status

--- components/menus/main.php
+++ components/menus/main.php
@@ -1,13 +1,18 @@
 <?php
 	$session = session::load();
-	$storage = storage::load();
+	$login = login::load();
 ?>
 <menu type="context" id="main_menu">
 	<menu label="Posts">
 		<menuitem label="about" data-request="load=contact_card"></menuitem>
 	</menu>
 	<menu label="Account">
+	<?php if($login->logged_in):?>
+		<menuitem label="Login" icon="images/icons/people.svgz" data-request="load_form=login" disabled></menuitem>
+		<menuitem label="Logout" icon="images/icons/people.svgz" data-request="action=logout"></menuitem>
+	<?php else:?>
 		<menuitem label="Login" icon="images/icons/people.svgz" data-request="load_form=login"></menuitem>
 		<menuitem label="Logout" icon="images/icons/people.svgz" data-request="action=logout" disabled></menuitem>
+	<?php endif?>
 	</menu>
 </menu>
