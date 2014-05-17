8aa5ddba4ceee969fbecf2d6b8b956ba58675e2d
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-05-17 10:39:26 -0700

    Form for creating new post

---
+++ components/forms/new_post.php
@@ -0,0 +1,21 @@
+<?php
+	$session = session::load();
+?>
+<form name="new_post" action="." method="post">
+	<fieldset form="new_post">
+		<legend>New Post</legend>
+		<label for="title">Title: </label>
+		<input type="text" name="title" id="title" required/><br/>
+		<label for="description">Description: </label>
+		<input type="text" name="description" id="description" required/><br/>
+		<label for="keywords">Keywords: </label>
+		<input type="text" name="keywords" id="keywords" required/><br/>
+		<label for="author">Author: </label>
+		<input type="text" name="author" id="author" required/><br/>
+		<label for="content">Post: </label>
+		<textarea name="content" id="content" required></textarea><br/>
+		<input type="hidden" nome="nonce" value="<?=$session->nonce?>" required readonly/>
+		<input type="submit" value="Submit"/>
+		<input type="reset" value="Reset"/>
+	</fieldset>
+</form>
