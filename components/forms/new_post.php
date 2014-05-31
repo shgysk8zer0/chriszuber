7ac15d56cb63119763b151cf6b6cd1b14ee3ed48
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-05-31 15:47:22 -0700

    Use contenteditable to write blog posts as HTML instead of using inputs

--- components/forms/new_post.php
+++ components/forms/new_post.php
@@ -4,16 +4,25 @@
 <form name="new_post" action="." method="post">
 	<fieldset form="new_post">
 		<legend>New Post</legend>
-		<label for="title">Title: </label>
-		<input type="text" name="title" id="title" required/><br/>
+		<article>
+			<header>
+				<h1 contenteditable="true" data-input-name="title" itemprop="headline">%TITLE%</h1>
+				<nav contenteditable="true" data-input-name="keywords">
+					%TAGS%
+				</nav>
+			</header>
+			<section contenteditable="true" data-input-name="content" itemprop="text">%CONTENT%</section>
+		</article>
+		<!--<label for="title">Title: </label>
+		<input type="text" name="title" id="title" required/><br/>-->
 		<label for="description">Description: </label>
 		<input type="text" name="description" id="description" required/><br/>
-		<label for="keywords">Keywords: </label>
-		<input type="text" name="keywords" id="keywords" required/><br/>
+		<!--<label for="keywords">Keywords: </label>
+		<input type="text" name="keywords" id="keywords" required/><br/>-->
 		<label for="author">Author: </label>
 		<input type="text" name="author" id="author" required/><br/>
-		<label for="content">Post: </label>
-		<textarea name="content" id="content" required></textarea><br/>
+		<!--<label for="content">Post: </label>
+		<textarea name="content" id="content" required></textarea><br/>-->
 		<input type="hidden" name="nonce" value="<?=$session->nonce?>" required readonly/>
 		<input type="submit" value="Submit"/>
 		<input type="reset" value="Reset"/>
