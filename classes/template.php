f4ebf4937dfe49098577ee2ebc5b08d4843e4fa4
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-05-31 15:06:04 -0700

    New class template which extends regexp.
    Loads the file from templates directory, adding the .tpl extension.
    
    This file is then used to construct a regular expression.
    
    __set() method is used to replace()-with():
    $template->title = 'Hello World' does $template->replace('%title')->with(
    'Hello World')
    
    set() method (no '__') accepts an array and replaces key with value.
    $template->set(['title' => 'Hello World']) does
    $template->replace('%title')->with('Hello World')
    
    out() method echos $template->execute(false), and clears replacements.
    execute(false) executes the replacement, but does not update the source

---
+++ classes/template.php
@@ -0,0 +1,45 @@
+<?php
+	class template extends regexp{
+		private static $instance = [];
+		private $template, $path;
+
+		public static function load($tpl) {
+			/**
+			 * Static load function avoids creating multiple instances/connections
+			 * It checks if an instance has been created and returns that or a new instance
+			 *
+			 * @params void
+			 * @return storage object/class
+			 * @example $storage = storage::load
+			 */
+
+			if(!array_key_exists($tpl, self::$instance)) {
+				self::$instance[$tpl] = new self($tpl);
+			}
+			return self::$instance[$tpl];
+		}
+
+		public function __construct($tpl) {
+			$this->path = BASE . "/components/templates/{$tpl}.tpl";
+			if(file_exists($this->path)) {
+				parent::__construct(file_get_contents($this->path));
+			}
+		}
+
+		public function __set($replace, $with) {
+			$this->replace('%' . trim(strtoupper($replace)) . '%')->with($with);
+		}
+
+		public function set($arr, $encloser = '%') {
+			foreach($arr as $replace => $with) {
+				$this->replace($encloser . trim(strtoupper($replace)) . $encloser)->with($with);
+			}
+			return $this;
+		}
+
+		public function out() {
+			echo $this->execute(false);
+			$this->pattern = $this->replacement = [];
+		}
+	}
+?>
