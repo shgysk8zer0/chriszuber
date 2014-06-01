7d1857418076d0ca87c49c4a3fd3fc4be747f961
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-01 15:28:45 -0700

    Updated pages class to use _pdo, ini, and template classes

--- classes/pages.php
+++ classes/pages.php
@@ -1,19 +1,9 @@
 <?php
 	class pages {
 		private static $instance = null;
-		private $pdo, $data = [];
-		public $content, $keywords, $description, $title, $author, $author_url, $created, $url, $kind;
+		public $data, $content, $path, $type, $url, $status;
 
 		public static function load() {
-			/**
-			 * Static load function avoids creating multiple instances/connections
-			 * It checks if an instance has been created and returns that or a new instance
-			 *
-			 * @params void
-			 * @return posts object/class
-			 * @example $session = session::load([$site])
-			 */
-
 			if(is_null(self::$instance)) {
 				self::$instance = new self();
 			}
@@ -21,151 +11,107 @@
 		}
 
 		public function __construct() {
-			$this->pdo = _pdo::load();
-			$this->path = explode('/', substr($_SERVER['REDIRECT_URL'], 1));
-			switch(strtolower($this->path[0])) {
-				case '':
-					$this->kind = 'home';
-					$results = $this->pdo->fetch_array('SELECT * FROM `posts` ORDER BY `created` LIMIT 1', 0);
-					$this->title = $results->title;
-					$this->content = $results->content;
-					$this->keywords = explode(',', $results->keywords);
-					$this->description = $results->description;
-					$this->author = $results->author;
-					$this->author_url = $results->url;
-					$this->created = $results->created;
-					$this->url = $results->url;
-					break;
-				case 'posts':
-					$this->kind = 'posts';
-					$results = $this->pdo->prepare('SELECT * FROM `posts` WHERE `title` = :title LIMIT 1')->bind(['title' => $path[1]])->execute()->get_results(0);
-					$this->title = $results->title;
-					$this->content = $results->content;
-					$this->keywords = explode(',', $results->keywords);
-					$this->description = $results->description;
-					$this->author = $results->author;
-					$this->author_url = $results->url;
-					$this->created = $results->created;
-					$this->url = $results->url;
-					break;
-				case 'tags':
-					$this->kind = 'tags';
-					$results = $this->pdo->prepare("SELECT * FROM `posts` WHERE `keywords` LIKE :tag")->bind(['tag' => "{$path[1]}"])->execute()->get_results();
-					break;
-				default:
-					$this->kind = $this->path[0];
-					$results = $this->pdo->prepare("SELECT * FROM :table WHERE `url` = :url")->bind(['table' => $post[0], 'url' => $post[1]])->execute()->get_results(0);
-			}
-			/*if($this->path[0] !== '') {
-				$this->kind = 'home';
-				$results = $this->pdo->fetch_array('SELECT * FROM `posts` ORDER BY `created` LIMIT 1', 0);
-			}
-			elseif(strtolower($this->path[0]) === 'tags') {
-				$this->kind = 'tags';
-				$results = $this->pdo->prepare("SELECT * FROM `tags` WHERE `keywords` LIKE :tag")->bind(['tag' => $post[1]])->execute()->get_results();
-			}
-			else {
-				$results = $this->pdo->prepare("SELECT * FROM :table WHERE `url` = :url")->bind(['table' => $post[0], 'url' => $post[0]])->execute()->get_results(0);
-			}*/
-		}
+			$connect = ini::load('connect');
+			$this->status = (array_key_exists('REDIRECT_STATUS', $_SERVER)) ? $_SERVER['REDIRECT_STATUS'] : http_response_code();
+			$pdo = _pdo::load();
+			$this->url = (array_keys_exist('REDIRECT_URL', 'REDIRECT_STATUS', $_SERVER)) ? $_SERVER['REDIRECT_URL'] : $_SERVER['REQUEST_URI'];
+			$this->path = explode('/', urldecode(preg_replace('/^(' . preg_quote(URL, '/')  .')?(' .preg_quote($connect->site, '/') . ')?(\/)?/', null, strtolower($this->url))));
 
-		private function build($results) {
-			$this->title = $results->title;
-			$this->content = $rseults->content;
-			$this->keywords = $results->keywords;
-			$this->description = $results->description;
-			$this->author = $results->author;
-			debug($results);
-		}
+			switch($this->path[0]) {
+				case 'tags': {
+					$this->type = 'tags';
+					$this->data = $pdo->prepare("
+						SELECT `title`, `description`, `author`, `author_url`, `url`, `created`
+						FROM `posts`
+						WHERE `keywords` LIKE :tag
+						LIMIT 20
+					")->bind([
+						'tag' => "%{$this->path[1]}%"
+					])->execute()->get_results();
+				} break;
 
-		public function __set($key, $value) {
-			/**
-			 * Setter method for the class.
-			 *
-			 * @param string $key, mixed $value
-			 * @return void
-			 * @example "$storage->key = $value"
-			 */
-
-			$key = preg_replace('/_/', '-', preg_quote($key, '/'));
-			$this->data[$key] = $value;
+				default: {
+					$this->type = 'posts';
+					if(count($this->path) === 1){
+						$this->data = $pdo->fetch_array('
+							SELECT *
+							FROM `posts`
+							WHERE `url` = ""
+							LIMIT 1
+						', 0);
+					}
+					else {
+						$this->data = $pdo->prepare('
+							SELECT *
+							FROM `posts`
+							WHERE `url` = :title
+							ORDER BY `created`
+							LIMIT 1
+						')->bind([
+							'title' => $this->path[1]
+						])->execute()->get_results(0);
+					}
+				}
+			}
+			if($this->data) $this->get_content();
 		}
 
 		public function __get($key) {
-			/**
-			 * The getter method for the class.
-			 *
-			 * @param string $key
-			 * @return mixed
-			 * @example "$storage->key" Returns $value
-			 */
-
-			$key = preg_replace('/_/', '-', preg_quote($key, '/'));
-			if(array_key_exists($key, $this->data)) {
-				return $this->data[$key];
-			}
-			return false;
+			return $this->data->$key;
 		}
 
-		public function __isset($key) {
-			/**
-			 * @param string $key
-			 * @return boolean
-			 * @example "isset({$storage->key})"
-			 */
+		public function get_content() {
+			switch($this->type) {
+				case 'posts': {
+					$template = template::load('posts');
+					$time = new simple_date($this->data->created);
+					$keywords = explode(',', $this->data->keywords);
+					$tags = [];
+					//foreach($keywords as $tag) $tags[] = '<a href="' . URL . '/tags/' . trim(strtolower(preg_replace('/\s/', '-', trim($tag)))) . '">' . trim(caps($tag)) . "</a>";
+				foreach($keywords as $tag) $tags[] = '<a href="' . URL . '/tags/' . strtolower(urlencode(trim($tag))) . '">' . trim(caps($tag)) . "</a>";
+					$this->content = $template->set([
+						'title' => $this->data->title,
+						'tags' => join(PHP_EOL, $tags),
+						'content' => $this->data->content,
+						'author' => $this->data->author,
+						'author_url' => $this->data->author_url,
+						'date' => $time->out('m/d/Y'),
+						'datetime' => $time->out()
+					])->out();
+				} break;
 
-			return array_key_exists(preg_replace('/_/', '-', $key), $this->data);
-		}
+				case 'tags': {
+					//$tag = preg_replace('/\-/', ' ', $path[1]);
+					$this->content = '<div class="tags">';
+					/*$posts = $DB->prepare("
+						SELECT `title`, `description`, `author`, `author_url`, `url`, `created`
+						FROM `posts`
+						WHERE `keywords` LIKE :tag
+						LIMIT 20
+					")->bind([
+						'tag' => "%{$path[1]}%"
+					])->execute()->get_results();*/
 
-		public function __unset($index) {
-			/**
-			 * Removes an index from the array.
-			 *
-			 * @param string $key
-			 * @return void
-			 * @example "unset($storage->key)"
-			 */
+					$template = template::load('tags');
 
-			unset($this->data[preg_replace('/_/', '-', $index)]);
-		}
-
-		public function __call($name, $arguments) {
-			/**
-			 * Chained magic getter and setter
-			 * @param string $name, array $arguments
-			 * @example "$storage->[getName|setName]($value)"
-			 */
-
-			$name = strtolower($name);
-			$act = substr($name, 0, 3);
-			$key = preg_replace('/_/', '-', substr($name, 3));
-			switch($act) {
-				case 'get':
-					if(array_key_exists($key, $this->data)) {
-						return $this->data[$key];
+					foreach($this->data as $post) {
+						$datetime = new simple_date($post->created);
+						$this->content .= $template->set([
+							'title' => $post->title,
+							'description' => $post->description,
+							'author' => $post->author,
+							'author_url' => $post->author_url,
+							'url' => $post->url,
+							'date' => $datetime->out('D M jS, Y \a\t h:iA')
+						])->out();
 					}
-					else{
-						die('Unknown variable.');
-					}
-					break;
-				case 'set':
-					$this->data[$key] = $arguments[0];
-					return $this;
-					break;
-				default:
-					die('Unknown method.');
+					$this->content .= '</div>';
+				}
 			}
 		}
 
-		public function keys() {
-			/**
-			 * Returns an array of all array keys for $thsi->data
-			 *
-			 * @param void
-			 * @return array
-			 */
-
-			return array_keys($this->data);
+		public function debug() {
+			debug($this);
 		}
 	}
 ?>
