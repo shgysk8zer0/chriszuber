be43ad46779ea29f7a845289383c2586b5c506ff
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-01 16:01:19 -0700

    Set placeholder, maxlength, and required attributes for description

--- components/forms/new_post.php
+++ components/forms/new_post.php
@@ -14,7 +14,7 @@
 			<section contenteditable="true" data-input-name="content" itemprop="text">Article Content Here</section>
 		</article>
 		<label for="description">Description: </label>
-		<textarea name="description" id="description"></textarea><br/>
+		<textarea name="description" id="description" placeholder="Description will appear in searches. 160 character limit" maxlength="160" required></textarea><br/>
 		<!--<input type="text" name="description" id="description" required/><br/>-->
 		<input type="hidden" name="nonce" value="<?=$session->nonce?>" required readonly/>
 		<input type="submit" value="Submit"/>
