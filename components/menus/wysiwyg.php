091ef94e77814b621c04617d53f19cd4b5ca121b
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-01 20:56:34 -0700

    New contextmenu for WYSIWYG text editing.
    Uses new selection class, which is buggy.
    
    Including as it is useful still, and I plan on working out the bugs.

---
+++ components/menus/wysiwyg.php
@@ -0,0 +1,10 @@
+<menu type="context" id="wysiwyg_menu">
+	<menu label="Text Style">
+		<menuitem label="Bold" data-style="bold"></menuitem>
+		<menuitem label="Italics" data-style="italics"></menuitem>
+		<menuitem label="Underline" data-style="underline"></menuitem>
+		<menuitem label="Strike Through" data-style="strike"></menuitem>
+		<menuitem label="Superscript" data-style="sup"></menuitem>
+		<menuitem label="Subscript" data-style="sub"></menuitem>
+	</menu>
+</menu>
