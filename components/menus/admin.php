a7b2f3abb6d0c1781f9fd606bbd5da526e269d46
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-01 20:49:36 -0700

    Added Edit Post and Restore Database to admin menu.

--- components/menus/admin.php
+++ components/menus/admin.php
@@ -3,4 +3,6 @@
 ?>
 <menu type="context" id="admin_menu">
 	<menuitem label="New Post" icon="images/icons/db.svgz" data-request="load_form=new_post"></menuitem>
+	<menuitem label="Edit Post" icon="images/icons/db.svgz"></menuitem>
+	<menuitem label="Restore Database" icon="images/icons/db.svgz" data-request="action=restore database"></menuitem>
 </menu>
