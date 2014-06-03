1b0d3a767fd2f836e70542656c2145f6b56d1f71
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-02 18:41:39 -0700

    Add submenu for edit/new post. Only load debugging content if admin

--- components/menus/admin.php
+++ components/menus/admin.php
@@ -22,13 +22,16 @@
 
 	$session = session::load();
 	$pdo = _pdo::load();
+	$login = login::load();
 
 	$tables = $pdo->show_tables();
 ?>
 <menu type="context" id="admin_menu">
-	<menuitem label="New Post" icon="images/icons/db.svgz" data-request="load_form=new_post"></menuitem>
-	<menuitem label="Edit Post" icon="images/icons/db.svgz"></menuitem>
-	<!--<menuitem label="Restore Database" icon="images/icons/db.svgz" data-request="action=restore database"></menuitem>-->
+	<menu label="Post Management">
+		<menuitem label="New Post" icon="images/icons/db.svgz" data-request="load_form=new_post"></menuitem>
+		<menuitem label="Edit Post" icon="images/icons/db.svgz"></menuitem>
+	</menu>
+	<?php if($login->role === 'admin'):?>
 	<menu label="PHP Defaults">
 			<menuitem type="command" label="_SERVER" icon="images/icons/coffee.svgz" data-request="debug=_SERVER&nonce=<?=$session->nonce?>" data-cache="debug_server"></menuitem>
 			<menuitem type="command" label="_SESSION" icon="images/icons/coffee.svgz" data-request="debug=_SESSION&nonce=<?=$session->nonce?>"></menuitem>
@@ -37,12 +40,12 @@
 			<menuitem type="command" label="PHP Extensions" icon="images/icons/coffee.svgz" data-request="debug=extensions&nonce=<?=$session->nonce?>" data-cache="debug_extensions"></menuitem>
 			<menuitem type="command" label="Apache Modules" icon="images/icons/coffee.svgz" data-request="debug=modules&nonce=<?=$session->nonce?>" data-cache="debug_modules"></menuitem>
 		</menu>
-		<menu label="Manage Database">
-			<menuitem type="command" label="Restore Database" icon="images/icons/db.svgz" data-request="action=restore database&nonce=<?=$session->nonce?>" data-confirm="Are you sure you want to restore the database from mother_brain.sql? All changes made since the last mysqldump will be reverted."></menuitem>
-			<menuitem type="command" label="Backup Database" icon="images/icons/db.svgz" data-request="action=backup database&nonce=<?=$session->nonce?>" data-confirm="Are you sure you want to backup the database to mother_brain.sql?"></menuitem>
-			<menuitem type="command" label="Clear CSP" icon="images/icons/db.svgz" data-request="reset_table=CSP_errors&nonce=<?=$session->nonce?>"></menuitem>
-			<menuitem type="command" label="MySQL Query" icon="images/icons/db.svgz" data-request="action=mysql_query&nonce=<?=$session->nonce?>" data-prompt="Type your query:"></menuitem>
-			</menu>
-		<menuitem type="command" label="test" icon="images/icons/coffee.svgz" data-request="action=test"></menuitem>
-		<menuitem type="command" label="Clear Cache" icon="images/icons/settings.svgz" data-confirm="Are you sure you want to clear your cache for this site?"></menuitem>
+	<menu label="Manage Database">
+		<menuitem type="command" label="Restore Database" icon="images/icons/db.svgz" data-request="action=restore database&nonce=<?=$session->nonce?>" data-confirm="Are you sure you want to restore the database from mother_brain.sql? All changes made since the last mysqldump will be reverted."></menuitem>
+		<menuitem type="command" label="Backup Database" icon="images/icons/db.svgz" data-request="action=backup database&nonce=<?=$session->nonce?>" data-confirm="Are you sure you want to backup the database to mother_brain.sql?"></menuitem>
+		<menuitem type="command" label="Clear CSP" icon="images/icons/db.svgz" data-request="reset_table=CSP_errors&nonce=<?=$session->nonce?>"></menuitem>
+		<menuitem type="command" label="MySQL Query" icon="images/icons/db.svgz" data-request="action=mysql_query&nonce=<?=$session->nonce?>" data-prompt="Type your query:"></menuitem>
+		</menu>
+	<menuitem type="command" label="test" icon="images/icons/coffee.svgz" data-request="action=test"></menuitem>
+	<menuitem type="command" label="Clear Cache" icon="images/icons/settings.svgz" data-confirm="Are you sure you want to clear your cache for this site?"></menuitem>
 </menu>
