469348159a774fbd6ff4a73e9d958457af46a30f
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-02 21:09:26 -0700

    Now with actual functions!
    get_all_tags loads all keywords from database, then explodes all of them on
    ','. Returns array_unique()
    
    get_recent_posts() returns an array of the most recent posts (ORDER BY
    `created`). Has optional arguments for limit and columns to select on.
    Default limit is 5, and default columns are title, url, and description.
    
    get_datalist() creates datalists. Uses a switch on $list to determine what
    to generate. Currently only has a case for tags, which uses get_all_tags().

---
+++ custom.php
@@ -0,0 +1,51 @@
+<?php
+	function get_all_tags(){
+		$pdo = _pdo::load();
+		$keywords = flatten($pdo->fetch_array("
+			SELECT `keywords` FROM `posts`
+		"));
+		$tags = [];
+		foreach($keywords as $keyword) {
+			foreach(explode(',', $keyword) as $tag) {
+				$tags[] = trim($tag);
+			}
+		};
+		return array_unique($tags);
+	}
+
+	function get_recent_posts($n = 5, $sel = ['title', 'url', 'description']) {
+		$pdo = _pdo::load();
+		if(is_string($sel) and $sel !== '*') {
+			$sel = explode(',', $sel);
+		}
+		if(is_array($sel)) {
+			foreach($sel as &$col) {
+				$col = "`{$pdo->escape($col)}`";
+			}
+			$sel = join(', ', $sel);
+		}
+		return $pdo->fetch_array("
+			SELECT `title`, `url`, `description`
+			FROM `posts`
+			WHERE `url` != ''
+			ORDER BY `created`
+			LIMIT {$n}
+		");
+	}
+
+	function get_datalist($list) {
+		switch(strtolower($list)) {
+			case 'tags': {
+				$options = get_all_tags();
+			} break;
+		}
+		if(isset($options)) {
+			$datalist = "<datalist id=\"{$list}\">";
+			foreach($options as $option) {
+				$datalist .= "<option value=\"{$option}\">{$option}</option>";
+			}
+			$datalist .= "</datalist>";
+			return $datalist;
+		}
+	}
+?>
