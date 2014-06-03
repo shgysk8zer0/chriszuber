a948fdef2af59098dd7c47ba666d71717c9b878d
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-02 21:15:21 -0700

    Method is now get, and rely on ajax() to download datalist.
    Removed hidden input for nonce since I'd like for this to be searchable
    from external sources.

--- components/forms/tag_search.php
+++ components/forms/tag_search.php
@@ -1,22 +1,9 @@
 <?php
 	$session = session::load();
-	$keywords = flatten($DB->fetch_array("
-		SELECT `keywords` FROM `posts`
-	"));
-	$tags = [];
-	foreach($keywords as $keyword) {
-		foreach(explode(',', $keyword) as $tag) {
-			$tags[] = trim($tag);
-		}
-	};
+	require_once('./custom.php');
 ?>
-<form name="tag_search" method="POST" actiion="<?=URL?>" autocomplete="off">
-	<input type="search" name="tag" placeholder="Search for tags" list="tags" pattern="\w+" required/>
-	<input type="hidden" name="nonce" value="<?=$session->nonce?>" required readonly/>
+<form name="tag_search" method="GET" action="<?=URL?>" autocomplete="off">
+	<input type="search" name="tags" placeholder="Search for tags" list="tags" pattern="[\w ]+" required/>
+	<!--<input type="hidden" name="nonce" value="<?=$session->nonce?>" required readonly/>-->
 	<button type="submit" data-icon="L"></button>
-	<datalist id="tags">
-		<?php foreach(array_unique($tags) as $tag): ?>
-		<option value="<?=$tag?>"><?=$tag?></option>
-		<?php endforeach?>
-	</datalist>
 </form>
