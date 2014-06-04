a3eda2d7b92e6d9e02e99c08b8fd6585c54780a5
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-03 17:10:53 -0700

    Updated wywiwyg editor for use with document.execCommand()
    No longer use getSelection() and substrings. Instead, use the built-in API
    for contenteditable.
    
    data-editor-command will be passed as the first argument for execCommand,
    null will always be passed as the second, and the third argument can be one
    of several things. If data-prompt is set, it will be the return from a prompt
    created, using data-prompt as its message (for links and such). If
    data-editor-value is set, it will use that instead. Finally, if none of these are
    true, it will be null.
    
    Added several more menuitems and menus, attempting to make better use
    of the full set of features.
    
    See https://developer.mozilla.org/en-US/docs/Midas

--- components/menus/wysiwyg.php
+++ components/menus/wysiwyg.php
@@ -1,10 +1,46 @@
+<!--https://developer.mozilla.org/en-US/docs/Midas-->
 <menu type="context" id="wysiwyg_menu">
 	<menu label="Text Style">
-		<menuitem label="Bold" data-style="bold"></menuitem>
-		<menuitem label="Italics" data-style="italics"></menuitem>
-		<menuitem label="Underline" data-style="underline"></menuitem>
-		<menuitem label="Strike Through" data-style="strike"></menuitem>
-		<menuitem label="Superscript" data-style="sup"></menuitem>
-		<menuitem label="Subscript" data-style="sub"></menuitem>
+		<menuitem label="Bold" data-editor-command="bold"></menuitem>
+		<menuitem label="Italics" data-editor-command="italic"></menuitem>
+		<menuitem label="Underline" data-editor-command="underline"></menuitem>
+		<menuitem label="Strike Through" data-editor-command="strikethrough"></menuitem>
+		<menuitem label="Big" data-editor-command="big"></menuitem>
+		<menuitem label="Small" data-editor-command="small"></menuitem>
+		<menuitem label="Superscript" data-editor-command="superscript"></menuitem>
+		<menuitem label="Subscript" data-editor-command="subscript"></menuitem>
+	</menu>
+	<menu label="Selection">
+		<menuitem label="Select All" data-editor-command="selectall"></menuitem>
+		<menuitem label="Clear Formatting" data-editor-command="removeformat"></menuitem>
+	</menu>
+	<menu label="Indentation">
+		<menuitem label="Increase" data-editor-command="indent"></menuitem>
+		<menuitem label="Decrease" data-editor-command="outdent"></menuitem>
+	</menu>
+	<menu label="Justify">
+		<menuitem label="Center" data-editor-command="justifycenter"></menuitem>
+		<menuitem label="Left" data-editor-command="justifyleft"></menuitem>
+		<menuitem label="Right" data-editor-command="justifyright"></menuitem>
+		<menuitem label="Full" data-editor-command="justifyfull"></menuitem>
+	</menu>
+	<menu label="Create">
+		<menu label="Headings">
+			<menuitem label="H1" data-editor-command="heading" data-editor-value="H1"></menuitem>
+			<menuitem label="H1" data-editor-command="heading" data-editor-value="H1"></menuitem>
+			<menuitem label="H1" data-editor-command="heading" data-editor-value="H1"></menuitem>
+			<menuitem label="H1" data-editor-command="heading" data-editor-value="H1"></menuitem>
+			<menuitem label="H1" data-editor-command="heading" data-editor-value="H1"></menuitem>
+			<menuitem label="H1" data-editor-command="heading" data-editor-value="H1"></menuitem>
+		</menu>
+		<menu label="List">
+			<menuitem label="Ordered" data-editor-command="insertorderedlist"></menuitem>
+			<menuitem label="Unordered" data-editor-command="insertunorderedlist"></menuitem>
+		</menu>
+		<menuitem label="link" data-editor-command="createlink" data-prompt="Enter link location"></menuitem>
+	</menu>
+	<menu label="History">
+		<menuitem label="Undo" data-editor-command="undo"></menuitem>
+		<menuitem label="Redo" data-editor-command="redo"></menuitem>
 	</menu>
 </menu>
