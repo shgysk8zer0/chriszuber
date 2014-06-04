af885ec3d644027ffa7c263e97a911e7c10f0ed0
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-03 18:21:33 -0700

    Updated to use dynamic instead of fixed code.
    Use placeholder %GOOGLE_ANALYTICS_CODE% and document.baseURI
    instead of the given analytics code and site.
    
    Will be using preg_replace() on %GOOGLE_ANALYTICS_CODE%

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-49193524-1', 'chriszuber.com');
+ga('create', '%GOOGLE_ANALYTICS_CODE%', document.baseURI);
 ga('send', 'pageview');
