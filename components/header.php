61adc122d41006c16d10f9e15b63df555d30ea8f
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-07 19:45:43 -0700

    Removed <menu type=list> an <li>'s from nav links

--- components/header.php
+++ components/header.php
@@ -12,11 +12,9 @@
 	<header>
 		<h1><a href="<?=URL?>" data-cache="home"><?=$storage->site_info->title?></a></h1>
 		<nav>
-			<menu type="list">
-				<?php foreach($posts as $post):?>
-				<li><a href="<?=URL?>/posts/<?=$post->url?>"><?=$post->title?></a></li>
-				<?php endforeach?>
-			</menu>
+			<?php foreach($posts as $post):?>
+			<a href="<?=URL?>/posts/<?=$post->url?>" itemprop="keywords"><?=$post->title?></a>
+			<?php endforeach?>
 			<?php load('forms/tag_search')?>
 		</nav>
 	</header>
