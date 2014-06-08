ec233e46204821f17af9f0ab98de73f5748dfe8f
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-07 23:35:50 -0700

    Do not need to load storage class anymore, so don't

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,3 @@
-<?php $storage = storage::load()?>
 	<footer>
 		<a href="https://github.com/shgysk8zer0/chriszuber" target="_blank" class="logo confirm">
 			<?php require('images/logos/github.svg')?>
