8d7728ce1de838024c1d7917d817ac5e83215d78
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-11 20:00:48 -0700

    Allow spaces in pattern for search input

--- components/forms/tag_search.php
+++ components/forms/tag_search.php
@@ -3,6 +3,6 @@
 	require_once('./custom.php');
 ?>
 <form name="tag_search" method="POST" action="<?=URL?>" role="search" autocomplete="off">
-	<input type="search" name="tags" placeholder="Search for tags" list="tags" pattern="[\w\-]+" rel="search" required/>
+	<input type="search" name="tags" placeholder="Search for tags" list="tags" pattern="[\w\- ]+" rel="search" required/>
 	<button type="submit" data-icon="L"></button>
 </form>
