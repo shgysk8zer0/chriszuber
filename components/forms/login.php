58d28ebef21cd6a1ae2aa45928c41276f83d8cda
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-15 12:25:17 -0700

    Fixed typo in placeholder for email in login form

--- components/forms/login.php
+++ components/forms/login.php
@@ -5,7 +5,7 @@
 	<fieldset form="login">
 		<legend>Login</legend>
 		<label for="user">Email: </label>
-		<input type="email" name="user" id="user" placeholder="user@exampple.com" required/>
+		<input type="email" name="user" id="user" placeholder="user@example.com" required/>
 		<label for="password">Password: </label>
 		<input type="password" name="password" id="password" pattern="<?=pattern('password')?>" required/>
 		<input type="hidden" name="nonce" value="<?=$session->nonce?>" required readonly/>
