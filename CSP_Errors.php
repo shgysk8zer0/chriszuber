967bef5adebdb013bafb0b70a4e489e48b6354c5
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-19 17:42:03 -0700

    Update CSP error reporting

--- CSP_Errors.php
+++ CSP_Errors.php
@@ -1,9 +1,10 @@
 <?php
 	if($_SERVER['HTTP_HOST'] === $_SERVER['SERVER_NAME'] and ($_SERVER['CONTENT_TYPE'] === 'application/json' or $_SERVER['CONTENT_TYPE'] === 'application/csp-report')){
-		require '../functions.php';
+		require_once('./functions.php');
 		$DB = new _pdo;
 		$log = 'csp_errors.log';
 		$JSON = file_get_contents('php://input');
+		file_put_contents('./csp.log', $JSON . PHP_EOL,  FILE_APPEND |  LOCK_EX );
 		$data = json_decode($JSON, true);
 		$report = <<<EOT
 BLOCKED: "{$data['csp-report']['blocked-uri']}" on "{$data['csp-report']['document-uri']}" due to rule "{$data['csp-report']['violated-directive']}".\n
