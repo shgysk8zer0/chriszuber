eb78d602afef354402a2a0c1c71cf7473e7f9d3a
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-06-25 18:07:31 -0700

    Use files.php (magic_cache class) for sending manifest.appcache
    Updated manifest.appcache to include all current resources.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	 * @license /LICENSE
 	 * @package chriszuber
 	 */
-	 
+
 	require_once('./functions.php');
 	config();
 	$DB = _pdo::load('connect');
@@ -24,7 +24,7 @@
 <!--[if lt IE 7]>      <html class="lt-ie9 lt-ie8 lt-ie7 no-js"> <![endif]-->
 <!--[if IE 7]>         <html class="lt-ie9 lt-ie8 no-js"> <![endif]-->
 <!--[if IE 8]>         <html class="lt-ie9 no-js"> <![endif]-->
-<!--[if gt IE 8]><!--> <html itemscope itemtype="http://schema.org/WebPage" class="no-js" <?php if(!localhost()):?> manifest="manifest.appcache"<?php endif?>> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html itemscope itemtype="http://schema.org/WebPage" class="no-js" <?php if(!localhost()):?> manifest="files.php?file=manifest.appcache"<?php endif?>> <!--<![endif]-->
 <!--<?=date('Y-m-d H:i:s')?>-->
 <?php load('head');?>
 <body lang="en" contextmenu="main_menu">
