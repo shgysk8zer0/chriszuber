4cf07e3851bd4f1de91cd4dacb6a2efa9b86924c
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-07-09 21:42:43 -0700

    Use <input> instead of [contenteditable] for title and keywords.

--- components/forms/new_post.php
+++ components/forms/new_post.php
@@ -3,10 +3,8 @@
 		<legend>New Post</legend>
 		<article contextmenu="wysiwyg_menu">
 			<header>
-				<h1 contenteditable="true" data-input-name="title" itemprop="headline">TITLE</h1>
-				<nav contenteditable="true" data-input-name="keywords">
-					KEYWORDS
-				</nav>
+				<h1 itemprop="headline"><input type="text" name="title" autocomplete="off" placeholder="Title" required/></h1>
+				<nav><input type="text" name="keywords" list="tags" autocomplete="off" placeholder="Keywords" requied/></nav>
 			</header>
 			<section contenteditable="true" data-input-name="content" itemprop="text" data-dropzone="main" data-menu="wysiwyg">Article Content Here</section>
 		</article>
