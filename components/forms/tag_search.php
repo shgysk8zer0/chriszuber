aebfe2489ef44f7bf63fec53ab6cccd27b8d9391
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-07-09 21:42:16 -0700

    Use correct id on <input> to match <label>

--- components/forms/tag_search.php
+++ components/forms/tag_search.php
@@ -1,5 +1,5 @@
 <form name="tag_search" method="POST" action="<?=URL?>" role="search" autocomplete="off">
 	<label for="tags_input" data-icon=","></label>
-	<input type="search" id="tags_search" name="tags" placeholder="Search for tags" list="tags" pattern="[\w\- ]+" rel="search" required/>
+	<input type="search" id="tags_input" name="tags" placeholder="Search for tags" list="tags" pattern="[\w\- ]+" rel="search" required/>
 	<button type="submit" data-icon="L"></button>
 </form>
