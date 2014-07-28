28d75554ef6e3084937dc4c8b328378be6b0ff8e
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-07-28 00:36:05 -0700

    Load login dialog, but do not open it on page load.

--- components/main.php
+++ components/main.php
@@ -3,5 +3,5 @@
 ?>
 <main role="main" itemprop="mainContentofPage" itemscope itemtype="http://schema.org/Blog" <?=($login->logged_in) ? ' contextmenu="admin_menu"' : ''?>>
 	<?=$pages->content?>
-	<?php load('sidebar')?>
+	<?php load('forms/login','sidebar')?>
 </main>
