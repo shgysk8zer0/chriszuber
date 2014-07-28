34673692704204fd8de8f23c92100dcfd454251a
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-07-28 00:35:46 -0700

    Login form is now a dialog.

--- components/forms/login.php
+++ components/forms/login.php
@@ -1,11 +1,14 @@
-<form name="login" action="." method="post">
-	<fieldset form="login">
-		<legend>Login</legend>
-		<label for="user"data-icon="@"></label>
-		<input type="email" name="user" id="user" placeholder="user@example.com" autofocus required/><br />
-		<label for="password" data-icon="x"></label>
-		<input type="password" name="password" id="password" pattern="<?=pattern('password')?>" required/>
-		<button type="submit" data-icon="X" title="Login"></button>
-		<button type="reset" data-icon="V" title="Reset Form"></button>
-	</fieldset>
-</form>
+<dialog id="loginDialog">
+	<form name="login" action="." method="post">
+		<fieldset form="login">
+			<legend>Login</legend>
+			<label for="user" data-icon="@"></label>
+			<input type="email" name="user" id="user" placeholder="user@example.com" autofocus required/><br />
+			<label for="password" data-icon="x"></label>
+			<input type="password" name="password" id="password" pattern="<?=pattern('password')?>" required/>
+			<button type="submit" data-icon="X" title="Login"></button>
+			<button type="reset" data-icon="V" title="Reset Form"></button>
+			<button type="button" title="Close Login" data-close="#loginDialog"></button>
+		</fieldset>
+	</form>
+</dialog>
