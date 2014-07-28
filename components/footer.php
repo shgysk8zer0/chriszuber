47b49d10f00f45fee6447e8c9c163140466ae2d7
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-07-28 00:01:25 -0700

    Clicking on .copyleft shows copyleft dialog

--- components/footer.php
+++ components/footer.php
@@ -1,8 +1,9 @@
-	<footer>
-		<?php foreach($DB->fetch_array("SELECT `url`, `icon`, `alt` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
-		<a href="<?=$link->url?>" target="_blank" class="logo" title="<?=$link->alt?>">
-			<img src="<?=URL?>/images/<?=$link->icon?>" alt="<?=$link->alt?>"/>
-		</a>
-		<?php endforeach?>
-		<a href="https://www.gnu.org/copyleft/" target="_blank" title="Copyleft" class="copyleft"><?=date('Y')?></a>
-	</footer>
+<footer>
+	<?php load('copyleft')?>
+	<?php foreach($DB->fetch_array("SELECT `url`, `icon`, `alt` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
+	<a href="<?=$link->url?>" target="_blank" class="logo" title="<?=$link->alt?>">
+		<img src="<?=URL?>/images/<?=$link->icon?>" alt="<?=$link->alt?>"/>
+	</a>
+	<?php endforeach?>
+	<span title="Copyleft" class="copyleft" data-show="#copyleftDialog"><?=date('Y')?></span>
+</footer>
