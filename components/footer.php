00d27962937fd47eac5c2a09e97183e7aceb60f4
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-07-27 19:55:55 -0700

    Get footer links and images from database. Loop through.

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,7 @@
 	<footer>
-		<a href="https://github.com/shgysk8zer0/chriszuber" target="_blank" class="logo confirm">
-			<?php require('images/logos/github.svg')?>
+		<?php foreach($DB->fetch_array("SELECT `url`, `icon` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
+		<a href="<?=$link->url?>" target="_blank" class="logo">
+			<img src="<?=URL?>/images/<?=$link->icon?>" alt="<?=$link->url?>"/>
 		</a>
+		<?php endforeach?>
 	</footer>
