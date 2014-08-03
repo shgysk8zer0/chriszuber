8502d1ccd525a680d9b136eade0029bfda8b0586
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-03 12:02:26 -0700

    Added placeholder to password input

--- components/forms/login.php
+++ components/forms/login.php
@@ -5,7 +5,7 @@
 			<label for="user" data-icon="@"></label>
 			<input type="email" name="user" id="user" placeholder="user@example.com" required/><br />
 			<label for="password" data-icon="x"></label>
-			<input type="password" name="password" id="password" pattern="<?=pattern('password')?>" required/>
+			<input type="password" name="password" id="password" pattern="<?=pattern('password')?>" placeholder="What's the password" required/>
 			<button type="submit" data-icon="X" title="Login"></button>
 			<button type="reset" data-icon="V" title="Reset Form"></button>
 			<button type="button" title="Close Login" data-close="#loginDialog"></button>
