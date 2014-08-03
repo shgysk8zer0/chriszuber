1366549ee8ee587fd3f4a9bf0e9939d5b186f3e5
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-03 13:07:58 -0700

    Update... Works now

--- CSP_Errors.php
+++ CSP_Errors.php
@@ -1,17 +1,34 @@
 <?php
-	if($_SERVER['HTTP_HOST'] === $_SERVER['SERVER_NAME'] and ($_SERVER['CONTENT_TYPE'] === 'application/json' or $_SERVER['CONTENT_TYPE'] === 'application/csp-report')){
+	if($_SERVER['HTTP_HOST'] === $_SERVER['SERVER_NAME'] and ($_SERVER['CONTENT_TYPE'] === 'application/json' or $_SERVER['CONTENT_TYPE'] === 'application/csp-report')) {
 		require_once('./functions.php');
-		$DB = new _pdo;
-		$log = 'csp_errors.log';
-		$JSON = file_get_contents('php://input');
-		file_put_contents('./csp.log', $JSON . PHP_EOL,  FILE_APPEND |  LOCK_EX );
-		$data = json_decode($JSON, true);
-		$report = <<<EOT
-BLOCKED: "{$data['csp-report']['blocked-uri']}" on "{$data['csp-report']['document-uri']}" due to rule "{$data['csp-report']['violated-directive']}".\n
-EOT;
-		//file_put_contents($log, $report, FILE_APPEND);
-		$DB->array_insert('CSP_errors', array('error' => $DB->escape($report)));
-		print_r($data['csp-report']);
+		$DB = _pdo::load();
+		$report = json_decode(trim(file_get_contents('php://input')), true)['csp-report'];
+
+		if(!array_key_exists('source-file', $report)) $report['source-file'] = 'unknown';
+		if(!array_key_exists('script-sample', $report)) $report['script-sample'] = '';
+
+		$DB->prepare("
+			INSERT INTO `CSP_errors` (
+				`blocked-uri`,
+				`document-uri`,
+				`violated-directive`,
+				`source-file`,
+				`script-sample`
+			)
+			VALUES (
+				:blocked_uri,
+				:document_uri,
+				:violated_directive,
+				:source_file,
+				:script_sample
+			)
+		")->bind([
+			'blocked_uri' => $report['blocked-uri'],
+			'document_uri' => $report['document-uri'],
+			'violated_directive' => $report['violated-directive'],
+			'source_file' => $report['source-file'],
+			'script_sample' => $report['script-sample']
+		])->execute();
 	}
 	else header("HTTP/1.1 403 Forbidden");
 ?>
