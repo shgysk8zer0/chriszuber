6bf7710c19eac148bfd4ff8564fd0529f2c346dc
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-03 13:16:31 -0700

    Remove commented out <img> in footer links.Just use <svg>

--- components/footer.php
+++ components/footer.php
@@ -2,7 +2,6 @@
 	<?php load('copyleft')?>
 	<?php foreach($DB->fetch_array("SELECT `url`, `icon`, `alt` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
 	<a href="<?=$link->url?>" target="_blank" class="logo" title="<?=$link->alt?>">
-		<!--<img src="<?=URL?>/images/<?=$link->icon?>" alt="<?=$link->alt?>"/>-->
 		<?php include(BASE . "/images/{$link->icon}")?>
 	</a>
 	<?php endforeach?>
