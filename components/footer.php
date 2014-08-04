e2a1013dc89f308d51ae7695c4c8afced33ec442
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-03 23:59:34 -0700

    Load contact dialog in footer

--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,9 @@
 <footer>
-	<?php load('copyleft')?>
 	<?php foreach($DB->fetch_array("SELECT `url`, `icon`, `alt` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
 	<a href="<?=$link->url?>" target="_blank" class="logo" title="<?=$link->alt?>">
 		<?php include(BASE . "/images/{$link->icon}")?>
 	</a>
 	<?php endforeach?>
+	<?php load('contact', 'copyleft')?>
 	<span title="Copyleft" class="copyleft" data-show-modal="#copyleftDialog"></span>
 </footer>
