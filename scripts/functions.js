520f6baf0cbf7fff813ce92e0fb6c5fec8b9cc53
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-03 19:54:17 -0700

    Close all dialogs on showModal()

--- scripts/functions.js
+++ scripts/functions.js
@@ -7,13 +7,16 @@ if (!window.Element) {
 	Element = function () {
 	};
 }
-if(!('show' in Element.prototype)) {
+if(!('showModal' in Element.prototype)) {
 	Element.prototype.show = function() {
 		this.setAttribute('open', '');
 	}
 	Element.prototype.showModal = function() {
 		var backdrop = document.createElement('div');
 		backdrop.classList.add('backdrop');
+		$('dialog[open]').each(function(dialog) {
+			dialog.close();
+		});
 		this.after(backdrop);
 		this.classList.add('modal');
 		this.show();
