7fc0902feae68308cdc72e363525edc63644eaa5
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-18 22:27:43 -0700

    Added hanlder for my hangman game

--- ajax.php
+++ ajax.php
@@ -865,24 +865,10 @@
 			} break;
 
 			case 'test': {
-				$str = 'testing <code><p>some text</p></code> and another <code><a href="#">link</a></code> testing';
 				$resp->notify(
-					str,
-					strip_tags(
-							preg_replace_callback(
-								'/(?<=\<code\>).*?(?=\<\/code\>)/',
-								function($code) {
-									return htmlentities($code[0]);
-								},
-								$str
-							),
-							'<br><p><span><div><a><ul><ol><li><i><u><b><em><u><h1><h2><h3><h4><h5><h6><pre><s><samp><strong><big><small><sup><sub><del><ins><code><var><kbd><cite>'
-						)
-				);
-				/*$resp->notify(
 					'Edit Me',
 					'I am on line ' . __LINE__ . ' in ' . __FILE__
-				);*/
+				);
 			}break;
 		}
 	}
@@ -927,6 +913,99 @@
 			'body > main',
 			ob_get_clean()
 		);
+	}
+
+	elseif(array_key_exists('hangman', $_REQUEST)) {
+		if(preg_match('/^[A-z]$/', $_REQUEST['hangman'])) {
+			if(preg_match("/{$_REQUEST['hangman']}/", $session->hangman_phrase)) {
+				$session->hangman_matches++;
+				preg_match_all("/{$_REQUEST['hangman']}/i", str_replace(' ', null, $session->hangman_phrase), $matches, PREG_OFFSET_CAPTURE);
+
+				foreach($matches[0] as $index) {
+					$pos = (int)$index[1] + 1;
+					$resp->text(
+						"#hangman_phrase > u:nth-of-type({$pos})",
+						$index[0]
+					);
+				}
+				$resp->disable(
+					"button[data-request=\"hangman={$_REQUEST['hangman']}\"]"
+				);
+				if((int)count(array_unique(str_split(str_replace(' ', null, $session->hangman_phrase)))) === (int)$session->hangman_matches) {
+					$resp->notify(
+						'Congratulation',
+						'You have won'
+					)->disable(
+						'button[data-request^="hangman"]:not([data-request="hangman=restart"])'
+					);
+				}
+			}
+			else {
+				$limbs = [
+					'head',
+					'torso',
+					'left_arm',
+					'right_arm',
+					'left_leg',
+					'right_leg'
+				];
+
+				if($session->hangman_bad_guesses >= count($limbs) - 1) {
+					$resp->notify(
+						'You lose',
+						'Try again?'
+					)->disable(
+						'button[data-request^="hangman"]:not([data-request="hangman=restart"])'
+					)->text(
+						'#hangman_phrase',
+						$session->hangman_phrase
+					)->attributes(
+						'#hangman_' . $limbs[$session->hangman_bad_guesses++],
+						'opacity',
+						1
+					);
+				}
+				else {
+					$resp->attributes(
+						'#hangman_' . $limbs[$session->hangman_bad_guesses++],
+						'opacity',
+						1
+					)->disable(
+						"button[data-request=\"hangman={$_REQUEST['hangman']}\"]"
+					);
+				}
+			}
+		}
+
+
+		else {
+			switch($_REQUEST['hangman']) {
+				case 'restart': {
+					$phrases = [
+						'this is the song that never ends',
+						'yes it goes on and on my friends',
+						'testing'
+					];
+
+					$session->hangman_phrase = strtoupper($phrases[mt_rand(0, count($phrases) - 1)]);
+					$session->hangman_matches = 0;
+					$session->hangman_bad_guesses = 0;
+
+					$resp->remove(
+						'#hangman_phrase'
+					)->after(
+						'section svg',
+						'<h1 id="hangman_phrase">' . preg_replace('/[A-z]/', '<u>&nbsp;&nbsp;</u>', $session->hangman_phrase) . '</h1>'
+					)->enable(
+						'button[data-request^="hangman"]'
+					)->attributes(
+						'svg path[id^=hangman]',
+						'opacity',
+						0
+					);
+				} break;
+			}
+		}
 	}
 
 	/*else {
