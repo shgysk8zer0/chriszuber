9c5f5246bd08e4cce468b2cd1beb3abd9bb6ee28
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-26 01:44:47 -0700

    Run module_test in main.php if enabled.
    If test passes, continune as usual. Otherwise, list out the arrays
    of missing modules.

--- components/main.php
+++ components/main.php
@@ -1,4 +1,11 @@
 <?php
+	$settings = ini::load('settings');
+	if(
+		isset($settings->module_test)
+		and $settings->module_test
+	) {
+		$missing = module_test();
+	}
 	if($DB->connected) {
 		$pages = pages::load();
 	}
@@ -7,7 +14,23 @@
 		$pages->content = null;
 	}
 ?>
-<main role="main" itemprop="mainContentofPage" itemscope itemtype="http://schema.org/Blog" <?=($login->logged_in) ? ' contextmenu="admin_menu"' : ''?>>
-	<?php load('sidebar')?>
-	<?=($DB->connected) ? $pages->content : null?>
+<main role="main" itemprop="mainContentofPage" itemscope itemtype="http://schema.org/Blog" <?=($login->logged_in and $login->role === 'admin') ? ' contextmenu="admin_menu"' : ''?>>
+	<?php
+		load('sidebar');
+		if(isset($missing)) {
+			echo '<div data-error="Missing Modules"><strong>Missing PHP Modules</strong><ul>';
+			foreach($missing->php as $php) {
+				echo "<li>{$php}</li>";
+			}
+			echo '</ul><strong>Missing Apache Modules</strong><ul>';
+			foreach($missing->apache as $apache) {
+				echo "<li>{$apache}</li>";
+			}
+			echo "</ul></div>";
+
+		}
+		elseif($DB->connected) {
+			echo $pages->content;
+		}
+	?>
 </main>
