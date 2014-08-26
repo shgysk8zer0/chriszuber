75450c9041420e4054e906cc09cc3e3b8916eb8e
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-08-26 01:38:54 -0700

    Send debug handler response to console.info()
    No print_r(), just $resp->info([$_POST['debug'], ...])

--- components/handlers/debug.php
+++ components/handlers/debug.php
@@ -3,29 +3,34 @@
 	check_nonce();
 	require_login('admin');
 
-	ob_start();
 	switch(trim($_POST['debug'])) {
 
 		case 'headers': {
-			debug(headers_list());
+			$resp->info([
+				'HTTP Headers' => headers_list()
+			]);
 		}break;
 
 		case 'extensions': {
-			debug(get_loaded_extensions());
+			$resp->info([
+				'PHP extensions' => get_loaded_extensions()
+			]);
 		}break;
 
 		case 'modules': {
-			debug(apache_get_modules());
+			$resp->info([
+				'Apache Modules' => apache_get_modules()
+			]);
 		}break;
 
 		default: {
-			debug($$_POST['debug']);
+			$resp->info([
+				$_POST['debug'] => $$_POST['debug']
+			]);
 		}
 	}
-	$resp->remove(
-		'main > :not(aside)'
-	)->prepend(
-		'body > main',
-		ob_get_clean()
+	$resp->notify(
+		'Debug info sent to console.info',
+		'Check your developer console'
 	);
 ?>
