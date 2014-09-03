74a8e29b16557461cdc5552cefdd8002fd62e350
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-09-03 01:24:42 -0700

    New string prototypes & add optional withCredentials to ajax()
    New string methods: contains(), startsWith(), & endsWith()

--- scripts/functions.js
+++ scripts/functions.js
@@ -29,6 +29,35 @@ if(!('showModal' in Element.prototype)) {
 		}
 	};
 }
+if ( !String.prototype.contains ) {
+	String.prototype.contains = function() {
+		return String.prototype.indexOf.apply( this, arguments ) !== -1;
+	};
+}
+if (!String.prototype.startsWith) {
+	Object.defineProperty(String.prototype, 'startsWith', {
+		enumerable: false,
+		configurable: false,
+		writable: false,
+		value: function (searchString, position) {
+			position = position || 0;
+			return this.lastIndexOf(searchString, position) === position;
+		}
+	});
+}
+if (!String.prototype.endsWith) {
+	Object.defineProperty(String.prototype, 'endsWith', {
+		value: function (searchString, position) {
+			var subjectString = this.toString();
+			if (position === undefined || position > subjectString.length) {
+				position = subjectString.length;
+			}
+			position -= searchString.length;
+			var lastIndex = subjectString.indexOf(searchString, position);
+			return lastIndex !== -1 && lastIndex === position;
+		}
+	});
+}
 (function(root) {
 	if (!root.CSS) {
 		root.CSS = {};
@@ -555,6 +584,10 @@ function ajax(data) {
 		else if(typeof navigator.onLine !== 'boolean' || navigator.onLine) {
 			var req = new XMLHttpRequest(),
 				progress = document.createElement('progress');
+			if (("withCredentials" in req) && ('withCredentials' in data)) {
+				req.withCredentials = data.withCredentials;
+
+			}
 			if(typeof data.contentType !== 'string') {
 				data.contentType = 'application/x-www-form-urlencoded';
 			}
