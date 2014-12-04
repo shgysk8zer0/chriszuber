542737ab11ca8221b082567f28570b8dc3221e30
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-03 23:08:37 -0800

    Added headers logging to webhooks

--- webhook.php
+++ webhook.php
@@ -1,7 +1,12 @@
 <?php
+	$headers = getallheaders();
 	$json = file_get_contents('php://input');
 	$data = json_decode($json);
 	$file = __DIR__ . DIRECTORY_SEPARATOR . 'hooks.json';
-	file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT) . PHP_EOL, FILE_APPEND | LOCK_EX);
+
+	file_put_contents($file, json_encode([
+		'headers' => $headers,
+		'data' => $data
+	], JSON_PRETTY_PRINT) . PHP_EOL, FILE_APPEND | LOCK_EX);
 	exit();
 ?>
