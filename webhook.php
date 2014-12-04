9b750a07f9a4cf905d7638b7c233ea8cd40741be
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-03 23:57:33 -0800

    Fixed order of setting vars and typos

--- webhook.php
+++ webhook.php
@@ -1,6 +1,5 @@
 <?php
 	error_reporting(0);
-	header('Content-Type: Application/JSON');
 	$headers = getallheaders();
 	if(
 		array_key_exists('content-type', $headers)
@@ -12,20 +11,22 @@
 		list($algo, $hash) = explode('=', $headers['X-Hub-Signature'], 2);
 		$json = file_get_contents('php://input');
 		$configFile = __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'github.json';
-		$logFileile = __DIR__ . DIRECTORY_SEPARATOR . $configFile->logFile;
 		$config = json_decode(file_get_contents($configFile));
+		$logFile = __DIR__ . DIRECTORY_SEPARATOR . $config->logFile;
 		$payloadHash = hash_hmac($algo, $json, $config->secret);
 		if($hash !== $payloadHash) {
 			http_response_code(404);
 			exit();
 		}
-		$data = json_decode($json);
+		else {
+			$data = json_decode($json);
 
-		file_put_contents(
-			$logFileile,
-			json_encode($data, JSON_PRETTY_PRINT) . PHP_EOL,
-			FILE_APPEND | LOCK_EX
-		);
+			file_put_contents(
+				$logFile,
+				json_encode($data, JSON_PRETTY_PRINT) . PHP_EOL,
+				FILE_APPEND | LOCK_EX
+			);
+		}
 	}
 	else {
 		http_response_code(404);
