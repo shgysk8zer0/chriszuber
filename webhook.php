d831809d5c2c8f89dedba50316fe4e765ad85910
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-04 02:24:25 -0800

    Fixed Typo

--- webhook.php
+++ webhook.php
@@ -1,9 +1,15 @@
 <?php
+	error_reporting();
 	set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'classes' . PATH_SEPARATOR . get_include_path());
 	spl_autoload_extensions('.class.php');
 	spl_autoload_register();
 
 	$webhook = new \core\GitHubWebhook('config/github.json');
-	header('content-type: text/plain');
-	print_r($webhook);
+	if($webhook->validate()) {
+		file_put_contents('hook.json', json_encode($webhook->parsed, JSON_PRETTY_PRINT), FILE_APPEND | LOCK_EX);
+	}
+	else {
+		http_response_code(404);
+		exit();
+	}
 ?>
