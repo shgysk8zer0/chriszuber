26495cd9e2bf5ffb92f74e02e7d449e0a51fa37b
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-06 19:12:14 +0000

    Needed to load functions.php to use flatten() in table class

--- webhook.php
+++ webhook.php
@@ -1,8 +1,6 @@
 <?php
 	error_reporting(0);
-	set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'classes' . PATH_SEPARATOR . get_include_path());
-	spl_autoload_extensions('.class.php');
-	spl_autoload_register();
+	require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 	header('Content-Type: text/plain');
 
 	$webhook = new \core\GitHubWebhook('config/github.json');
