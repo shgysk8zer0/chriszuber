3d1ca57dc13847f999fd1230d4105f97c1bfb291
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-07 01:56:56 -0800

    Restructure & reorder footer elements

--- components/default/footer.php
+++ components/default/footer.php
@@ -1,14 +1,12 @@
 <footer>
-	<?php if($DB->connected):?>
-	<?php foreach($DB->fetch_array("SELECT `url`, `icon`, `alt` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
+	<?php if($DB->connected): foreach($DB->fetch_array("SELECT `url`, `icon`, `alt` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
 	<a href="<?=$link->url?>" target="_blank" class="logo" title="<?=$link->alt?>">
 		<?php include(BASE . "/images/{$link->icon}")?>
 	</a>
-	<?php endforeach?>
-	<span title="Recent Commits" class="logo" data-request="action=recent_commits"><?php include(BASE . '/images/logos/git.svg')?></span>
+<?php endforeach;  endif;?>
 	<span title="Email Me" class="logo" data-show-modal="#email_admin_dialog"><?php include(BASE . '/images/icons/envelope.svg')?></span>
-	<span title="contact Info" class="logo" data-show-modal="#contactDialog"><?php include(BASE . '/images/icons/people.svg')?></span>
-	<?php endif?>
+	<span title="Contact Info" class="logo" data-show-modal="#contactDialog"><?php include(BASE . '/images/icons/people.svg')?></span>
+	<span title="Recent Commits" class="logo" data-request="action=recent_commits"><?php include(BASE . '/images/logos/git.svg')?></span>
 	<span title="Show README" class="logo" data-show-modal="#README"><?php include(BASE . '/images/icons/info.svg')?></span>
 	<span title="Copyleft" class="logo" data-show-modal="#copyleftDialog"><?php include(BASE . '/images/logos/copyleft.svg')?></span>
 	<?php load('contact', 'copyleft', 'forms/email_admin')?>
