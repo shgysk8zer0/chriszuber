226c68825d18a4a677fc62d7edc956f602ba4d6d
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-09 01:22:53 -0800

    Changed footer link/button order
    Move GitHub Issues & Commits to after regular links & before email

--- components/default/footer.php
+++ components/default/footer.php
@@ -4,10 +4,10 @@
 		<?php include(BASE . "/images/{$link->icon}")?>
 	</a>
 <?php endforeach;  endif;?>
-	<span title="Email Me" class="logo" data-show-modal="#email_admin_dialog"><?php include(BASE . '/images/icons/envelope.svg')?></span>
-	<span title="Contact Info" class="logo" data-show-modal="#contactDialog"><?php include(BASE . '/images/icons/people.svg')?></span>
 	<span title="Recent Commits" class="logo" data-request="action=recent_commits"><?php include(BASE . '/images/logos/git.svg')?></span>
 	<span title="GitHub Issues" class="logo" data-request="action=github_issues"><?php include(BASE . '/images/icons/warning.svg')?></span>
+	<span title="Email Me" class="logo" data-show-modal="#email_admin_dialog"><?php include(BASE . '/images/icons/envelope.svg')?></span>
+	<span title="Contact Info" class="logo" data-show-modal="#contactDialog"><?php include(BASE . '/images/icons/people.svg')?></span>
 	<span title="Show README" class="logo" data-show-modal="#README"><?php include(BASE . '/images/icons/info.svg')?></span>
 	<span title="Copyleft" class="logo" data-show-modal="#copyleftDialog"><?php include(BASE . '/images/logos/copyleft.svg')?></span>
 	<?php load('contact', 'copyleft', 'forms/email_admin')?>
