6bdde19a94e150786b369f19ac9fab84f7b2aa71
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-08 20:55:48 -0800

    Apply updates to DB connection and enable pagination
    - Use $github instead of $github->database
    - Write out table in HTML instead of a `\core\table`
    - Get start position from `$_REQUEST`
    - `LIMIT {$start}, 10`
    - Pagination buttons (prev & next)

--- components/default/recent_commits.php
+++ components/default/recent_commits.php
@@ -1,28 +1,14 @@
 <?php
 	$github = json_decode(file_get_contents('config/github.json'));
-	$PDO = new \core\PDO($github->database);
+	$PDO = new \core\PDO($github);
 	$filename = filename(__FILE__);
-
-	$table = new \core\table('SHA', 'Commit', 'Author', 'Timestamp');
-	$table->caption = 'Recent Commits';
-
-	array_map(function($commit) use (&$table) {
-		$commit->Message = nl2br($commit->Message, false);
-		$commit->Message = htmlentities($commit->Message, ENT_QUOTES | ENT_HTML5, 'UTF-8', true);
-		$commit->Message = explode('&lt;br&gt;', $commit->Message);
-		$table->SHA(
-			"<code>{$commit->SHA}</code>"
-		)->Commit(
-			'<details><summary>' . array_shift($commit->Message) . '</summary>
-			<a href="' . $commit->URL .'" target="_blank">'
-			. join('<br>', $commit->Message) . '</a></details>'
-		)->Author(
-			$commit->Author
-		)->Timestamp(
-			"<time>{$commit->Timestamp}</time>"
-		)->next_row();
-	}, $PDO->fetch_array("
-		SELECT
+	$start = (
+		array_key_exists('commit_start', $_REQUEST)
+		and is_numeric($_REQUEST['commit_start'])
+	) ? (int)$_REQUEST['commit_start'] : 0;
+	if($start <0) $start = 0;
+	$end = $start + 10;
+	$commits = $PDO->prepare("SELECT
 			`SHA`,
 			`Commit_URL` AS `URL`,
 			`Commit_Message` AS `Message`,
@@ -31,10 +17,80 @@
 		FROM `Commits`
 		ORDER BY `Timestamp`
 		DESC
-		LIMIT 10
-	"));
+		LIMIT {$start}, 10;
+	")->execute()->get_results();
+
+	array_walk($commits, function(&$commit) {
+		$commit->Message = nl2br($commit->Message, false);
+		$commit->Message = htmlentities($commit->Message, ENT_QUOTES | ENT_HTML5, 'UTF-8', true);
+		$commit->Message = explode('&lt;br&gt;', $commit->Message);
+	});
 ?>
 <dialog id="<?=$filename?>_dialog">
 	<button type="button" data-delete="#<?=$filename?>_dialog"></button>
-	<?=$table->out(true, true);?>
+	<button type="button" title="Previous" role="prev" data-icon="<" data-request="action=recent_commits&commit_start=<?=$start - 10?>" data-delete="#<?=$filename?>_dialog"<?=($start > 0) ? null : ' disabled';?>></button>
+	<button type="button" title="Next" role="next" data-icon=">" data-request="action=recent_commits&commit_start=<?=$end?>" data-delete="#<?=$filename?>_dialog"<?=(count($commits) <10) ? ' disabled' : null;?>></button>
+	<br />
+	<table border="1">
+		<caption>
+			Recent commits to <?=$github->repository->name;?>
+		</caption>
+		<thead>
+			<tr>
+				<th>
+					SHA
+				</th>
+				<th>
+					Commit
+				</th>
+				<th>
+					Author
+				</th>
+				<th>
+					Timestamp
+				</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<th>
+					SHA
+				</th>
+				<th>
+					Commit
+				</th>
+				<th>
+					Author
+				</th>
+				<th>
+					Timestamp
+				</th>
+			</tr>
+		</tfoot>
+		<tbody>
+		<?php foreach($commits as $commit):?>
+			<tr>
+				<td>
+					<a href="<?=$commit->URL;?>" target="_blank">
+						<code><?=$commit->SHA;?></code>
+					</a>
+				</td>
+				<td>
+					<details>
+						<summary>
+							<?=array_shift($commit->Message)?>
+						</summary>
+						<kbd><?=join('<br>', $commit->Message);?></kbd>
+					</details>
+				</td>
+				<td>
+					<?=$commit->Author;?>
+				</td>
+				<td>
+					<time><?=$commit->Timestamp;?></time>
+				</td>
+			</tr>
+		<?php endforeach;?>
+		</tbody>
+	</table>
 </dialog>
