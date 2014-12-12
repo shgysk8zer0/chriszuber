63a50e2b55bd70c70fb5c68c060745a5f7a3c6c5
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-11 16:15:09 -0800

    Rewrite CSP Errors handler
    - Add in exception handlers, responses, etc
    - Self-handle all of this without requiring functions.php
    - Convert all header keys to lower case
    - Check content-type, content-length, and HTTP_HOST
    - Check contet-type using RegExp

--- CSP_Errors.php
+++ CSP_Errors.php
@@ -1,36 +1,91 @@
 <?php
-	if($_SERVER['HTTP_HOST'] === $_SERVER['SERVER_NAME'] and ($_SERVER['CONTENT_TYPE'] === 'application/json' or $_SERVER['CONTENT_TYPE'] === 'application/csp-report')) {
-		require_once('./functions.php');
-		$DB =\core\PDO::load('connect');
-		$report = json_decode(trim(file_get_contents('php://input')), true)['csp-report'];
+	error_reporting(0);
+	header('Content-Type: application/json');
+	set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'classes' . PATH_SEPARATOR . get_include_path() . PATH_SEPARATOR  . __DIR__  . DIRECTORY_SEPARATOR . 'config');
+	spl_autoload_extensions('.class.php');
+	spl_autoload_register();
+	define('BASE', __DIR__);
 
-		if(!array_key_exists('source-file', $report)) $report['source-file'] = 'unknown';
-		if(!array_key_exists('script-sample', $report)) $report['script-sample'] = '';
+	$report = trim(file_get_contents('php://input'));
+	$headers = getallheaders();
+	$headers = (object)array_combine(
+		array_map('strtolower', array_keys($headers)),
+		array_values($headers)
+	);
 
-		$DB->prepare("
-			INSERT INTO `CSP_errors` (
-				`blocked-uri`,
-				`document-uri`,
-				`violated-directive`,
-				`source-file`,
-				`script-sample`
-			)
-			VALUES (
-				:blocked_uri,
-				:document_uri,
-				:violated_directive,
-				:source_file,
-				:script_sample
-			)
-		")->bind([
-			'blocked_uri' => $report['blocked-uri'],
-			'document_uri' => $report['document-uri'],
-			'violated_directive' => $report['violated-directive'],
-			'source_file' => $report['source-file'],
-			'script_sample' => $report['script-sample']
-		])->execute();
+	try {
+		if($_SERVER['HTTP_HOST'] !== $_SERVER['SERVER_NAME']) {
+			throw new \Exception('Unauthorized', 401);
+		}
+		if(
+			preg_match('/^application\/(json|csp-report)/',
+			$headers->{'content-type'}
+		)) {
+			if(
+				is_null($headers->{'content-length'})
+				or strlen($report) !== (int)$headers->{'content-length'}
+			) {
+				throw new \Exception('Content-Length not set or invalid', 411);
+			}
+
+			$report = json_decode($report)->{'csp-report'};
+
+			if(!is_object($report) or empty($report)) {
+				throw new \Exception('Invalid csp-report', 400);
+			}
+			if(!isset($report->{'script-sample'})) $report->{'script-sample'} = '';
+			if(!isset($report->{'source-file'})) $report->{'source-file'} = null;
+
+			$DB =new \core\PDO('connect');
+
+			if(!$DB->connected) {
+				throw new \Exception('Database connection failed', 504);
+			}
+
+			$DB->prepare("INSERT INTO `CSP_errors` (
+					`blocked-uri`,
+					`document-uri`,
+					`violated-directive`,
+					`source-file`,
+					`script-sample`
+				) VALUES (
+					:blocked_uri,
+					:document_uri,
+					:violated_directive,
+					:source_file,
+					:script_sample
+				);
+			")->bind([
+				'blocked_uri' => $report->{'blocked-uri'},
+				'document_uri' => $report->{'document-uri'},
+				'violated_directive' => $report->{'violated-directive'},
+				'source_file' => $report->{'source-file'},
+				'script_sample' => $report->{'script-sample'}
+			]);
+
+			if(!$DB->execute()) {
+				throw new \Exception('Error not successfully recorded', 504);
+			}
+
+			else {
+				http_response_code(202);
+				exit(json_encode($report));
+			}
+		}
+
+		else {
+			throw new \Exception('Invalid Content-Type header', 405);
+		}
 	}
-	else {
-		http_response_code(403);
+
+	catch(\Exception $e) {
+		http_response_code($e->getCode());
+		exit(json_encode([
+			'Message' => $e->getMessage(),
+			'Request' => [
+				'Headers' => $headers,
+				'Body' => $report
+			]
+		]));
 	}
 ?>
