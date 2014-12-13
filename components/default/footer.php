83d8015c16aa78012b4d7e3f973ebd40c666e6f5
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-13 01:13:39 -0800

    Use SVG_use for all icons in footer
    Use the new SVG_use() function instead of manually writing out markup
    for icons

--- components/default/footer.php
+++ components/default/footer.php
@@ -1,26 +1,26 @@
 <footer>
 	<?php if($DB->connected): foreach($DB->fetch_array("SELECT `url`, `icon`, `alt` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
 	<a href="<?=$link->url?>" target="_blank" class="logo" title="<?=$link->alt?>">
-		<svg><use xlink:href="images/icons/combined.svg#<?=filename($link->icon);?>" /></svg>
+		<?=SVG_use(filename($link->icon));?>
 	</a>
 <?php endforeach;  endif;?>
 	<span title="Recent Commits" class="logo" data-request="action=recent_commits">
-		<svg><use xlink:href="images/icons/combined.svg#git" /></svg>
+		<?=SVG_use('git');?>
 	</span>
 	<span title="GitHub Issues" class="logo" data-request="action=github_issues">
-		<svg><use xlink:href="images/icons/combined.svg#issues-open" /></svg>
+		<?=SVG_use('issues-open');?>
 	</span>
 	<span title="Email Me" class="logo" data-show-modal="#email_admin_dialog">
-		<svg><use xlink:href="images/icons/combined.svg#envelope" /></svg>
+		<?=SVG_use('envelope');?>
 	</span>
 	<span title="Contact Info" class="logo" data-show-modal="#contactDialog">
-		<svg><use xlink:href="images/icons/combined.svg#people" /></svg>
+		<?=SVG_use('people');?>
 	</span>
 	<span title="Show README" class="logo" data-request="action=README">
-		<svg><use xlink:href="images/icons/combined.svg#info" /></svg>
+		<?=SVG_use('info');?>
 	</span>
 	<span title="Copyleft" class="logo" data-show-modal="#copyleftDialog">
-		<svg><use xlink:href="images/icons/combined.svg#copyleft" /></svg>
+		<?=SVG_use('copyleft');?>
 	</span>
 	<?php load('contact', 'copyleft', 'forms/email_admin')?>
 </footer>
