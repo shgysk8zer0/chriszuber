8d07ae295ed1ff2ac8490e6f994e2329b12af2d6
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-15 23:30:15 -0800

    Updated accessibility & formatting
    + Added role attribute to header
    + Broke title into multiple lines (adds spacing before logo)
    + Load install button or search form depending on db connect

--- components/default/header.php
+++ components/default/header.php
@@ -1,13 +1,16 @@
 <?php
 	$storage = \core\storage::load();
 ?>
-	<header>
-		<h1><a href="<?=URL?>" rel="bookmark"><?=$storage->site_info->title?></a></h1>
+	<header role="banner">
+		<h1>
+			<a href="<?=URL?>" rel="bookmark">
+				<?=$storage->site_info->title?>
+			</a>
+		</h1>
 		<nav role="navigation">
 			<?php foreach(get_recent_posts(5) as $post):?>
 			<a href="<?=URL?>/posts/<?=$post->url?>"><?=$post->title?></a>
 			<?php endforeach?>
 		</nav>
-		<?php load('forms/tag_search')?>
-		<?php if(!$DB->connected) load('buttons/install')?>
+		<?php load(($DB->connected) ? 'forms/tag_search' : 'buttons/install')?>
 	</header>
