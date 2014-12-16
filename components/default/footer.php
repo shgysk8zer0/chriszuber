866d101b6f475ef29f28077926558286ba8662f2
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-15 17:19:02 -0800

    Added icon class to several footer icons
    For all icons whose fill can be altered (no transparent fill)

--- components/default/footer.php
+++ components/default/footer.php
@@ -11,13 +11,13 @@
 		<?=SVG_use('issues-open');?>
 	</span>
 	<span title="Email Me" class="logo" data-show-modal="#email_admin_dialog">
-		<?=SVG_use('envelope');?>
+		<?=SVG_use('envelope', ['class' => 'icon']);?>
 	</span>
 	<span title="Contact Info" class="logo" data-show-modal="#contactDialog">
-		<?=SVG_use('people');?>
+		<?=SVG_use('people', ['class' => 'icon']);?>
 	</span>
 	<span title="Show README" class="logo" data-request="action=README">
-		<?=SVG_use('info');?>
+		<?=SVG_use('info', ['class' => 'icon']);?>
 	</span>
 	<span title="Copyleft" class="logo" data-show-modal="#copyleftDialog">
 		<?=SVG_use('copyleft');?>
