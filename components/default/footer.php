3058648e74eed037f1f992d8dc97d20d1d8ac1fa
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-15 20:06:09 -0800

    Rework footer links/icons
    Stay in PHP more...
    
    + Set icons & attributes in an array
    + Loop through array instead of setting individially in PHP + HTML
    + Formatting changes

--- components/default/footer.php
+++ components/default/footer.php
@@ -1,26 +1,57 @@
 <footer>
-	<?php if($DB->connected): foreach($DB->fetch_array("SELECT `url`, `icon`, `alt` FROM `footer_links` ORDER BY `order` ASC") as $link):?>
+	<?php if($DB->connected):
+		foreach($DB->fetch_array("SELECT
+			`url`,
+			`icon`,
+			`alt`
+		FROM `footer_links`
+		ORDER BY `order` ASC;
+	") as $link):?>
 	<a href="<?=$link->url?>" target="_blank" class="logo" title="<?=$link->alt?>">
 		<?=SVG_use(filename($link->icon));?>
 	</a>
-<?php endforeach;  endif;?>
-	<span title="Recent Commits" class="logo" data-request="action=recent_commits">
-		<?=SVG_use('git');?>
-	</span>
-	<span title="GitHub Issues" class="logo" data-request="action=github_issues">
-		<?=SVG_use('issues-open');?>
-	</span>
-	<span title="Email Me" class="logo" data-show-modal="#email_admin_dialog">
-		<?=SVG_use('envelope', ['class' => 'icon']);?>
-	</span>
-	<span title="Contact Info" class="logo" data-show-modal="#contactDialog">
-		<?=SVG_use('people', ['class' => 'icon']);?>
-	</span>
-	<span title="Show README" class="logo" data-request="action=README">
-		<?=SVG_use('info', ['class' => 'icon']);?>
-	</span>
-	<span title="Copyleft" class="logo" data-show-modal="#copyleftDialog">
-		<?=SVG_use('copyleft');?>
-	</span>
-	<?php load('contact', 'copyleft', 'forms/email_admin')?>
+	<?php
+		endforeach;
+		endif;
+
+		foreach([
+			'git' => [
+				'title' => 'Recent Commits',
+				'class' => 'logo',
+				'data-request' => "action=recent_commits"
+			],
+			'issues-open' => [
+				'title' => 'GitHub Issues',
+				'class' => 'logo',
+				'data-request' => 'action=github_issues'
+			],
+			'envelope' => [
+				'title' => 'Email Me',
+				'class' => 'logo',
+				'data-show-modal' => '#email_admin_dialog'
+			],
+			'people' => [
+				'title' => 'Contact Info',
+				'class' => 'logo',
+				'data-show-modal' => '#contactDialog'
+			],
+			'info' => [
+				'title' => 'Show README',
+				'class' => 'logo',
+				'data-request' => 'action=README'
+			],
+			'copyleft' => [
+				'title' => 'Copyleft',
+				'class' => 'logo',
+				'data-show-modal' => '#copyleftDialog'
+			]
+		] as $icon => $attributes) {
+			echo SVG_use($icon, $attributes);
+		}
+		load(
+			'contact',
+			'copyleft',
+			'forms/email_admin'
+		);
+	?>
 </footer>
