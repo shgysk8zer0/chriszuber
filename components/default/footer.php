9db2d36c0a39600e05baa0e11eec323b25a076ed
Chris Zuber <shgysk8zer0@gmail.com>
2015-01-08 19:34:04 -0800

    Footer links now add class currentColor
    Allows for icons to inherit color from CSS

--- components/default/footer.php
+++ components/default/footer.php
@@ -7,7 +7,7 @@
 		FROM `footer_links`
 		ORDER BY `order` ASC;
 	") as $link):?>
-	<a href="<?=$link->url?>" target="_blank" class="logo" title="<?=$link->alt?>">
+	<a href="<?=$link->url?>" target="_blank" class="logo currentColor" title="<?=$link->alt?>">
 		<?=SVG_use(filename($link->icon));?>
 	</a>
 	<?php
@@ -17,32 +17,32 @@
 		foreach([
 			'git-commit' => [
 				'title' => 'Recent Commits',
-				'class' => 'logo',
+				'class' => 'logo currentColor',
 				'data-request' => "action=recent_commits"
 			],
 			'issue-opened' => [
 				'title' => 'GitHub Issues',
-				'class' => 'logo',
+				'class' => 'logo currentColor',
 				'data-request' => 'action=github_issues'
 			],
 			'mail' => [
 				'title' => 'Email Me',
-				'class' => 'logo',
+				'class' => 'logo currentColor',
 				'data-show-modal' => '#email_admin_dialog'
 			],
 			'people' => [
 				'title' => 'Contact Info',
-				'class' => 'logo',
+				'class' => 'logo currentColor',
 				'data-show-modal' => '#contactDialog'
 			],
 			'question' => [
 				'title' => 'Show README',
-				'class' => 'logo',
+				'class' => 'logo currentColor',
 				'data-request' => 'action=README'
 			],
 			'copyleft' => [
 				'title' => 'Copyleft',
-				'class' => 'logo',
+				'class' => 'logo currentColor',
 				'data-show-modal' => '#copyleftDialog'
 			]
 		] as $icon => $attributes) :{?>
