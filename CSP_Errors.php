e730a85d9a154da7971cc83152b3d80b891d1a0c
Chris Zuber <shgysk8zer0@gmail.com>
2015-03-26 22:32:38 -0700

    Update CSP_Errors
    * No need for setting autoloader (done automatically)
    * PSR-2 compliance
    * MySQL queries start and end with their quotation marks (no extra NL)

--- CSP_Errors.php
+++ CSP_Errors.php
@@ -1,91 +1,86 @@
 <?php
-	error_reporting(0);
-	header('Content-Type: application/json');
-	set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'classes' . PATH_SEPARATOR . get_include_path() . PATH_SEPARATOR  . __DIR__  . DIRECTORY_SEPARATOR . 'config');
-	spl_autoload_extensions('.class.php');
-	spl_autoload_register();
-	define('BASE', __DIR__);
+error_reporting(0);
+header('Content-Type: application/json');
+define('BASE', __DIR__);
 
-	$report = trim(file_get_contents('php://input'));
-	$headers = getallheaders();
-	$headers = (object)array_combine(
-		array_map('strtolower', array_keys($headers)),
-		array_values($headers)
-	);
+$report = trim(file_get_contents('php://input'));
+$headers = getallheaders();
+$headers = (object)array_combine(
+	array_map('strtolower', array_keys($headers)),
+	array_values($headers)
+);
 
-	try {
-		if($_SERVER['HTTP_HOST'] !== $_SERVER['SERVER_NAME']) {
-			throw new \Exception('Unauthorized', 401);
-		}
+try {
+	if($_SERVER['HTTP_HOST'] !== $_SERVER['SERVER_NAME']) {
+		throw new \Exception('Unauthorized', 401);
+	}
+	if(
+		preg_match('/^application\/(json|csp-report)/',
+		$headers->{'content-type'}
+	)) {
 		if(
-			preg_match('/^application\/(json|csp-report)/',
-			$headers->{'content-type'}
-		)) {
-			if(
-				is_null($headers->{'content-length'})
-				or strlen($report) !== (int)$headers->{'content-length'}
-			) {
-				throw new \Exception('Content-Length not set or invalid', 411);
-			}
-
-			$report = json_decode($report)->{'csp-report'};
-
-			if(!is_object($report) or empty($report)) {
-				throw new \Exception('Invalid csp-report', 400);
-			}
-			if(!isset($report->{'script-sample'})) $report->{'script-sample'} = '';
-			if(!isset($report->{'source-file'})) $report->{'source-file'} = null;
-
-			$DB =new \shgysk8zer0\Core\PDO('connect.json');
-
-			if(!$DB->connected) {
-				throw new \Exception('Database connection failed', 504);
-			}
-
-			$DB->prepare("INSERT INTO `CSP_errors` (
-					`blocked-uri`,
-					`document-uri`,
-					`violated-directive`,
-					`source-file`,
-					`script-sample`
-				) VALUES (
-					:blocked_uri,
-					:document_uri,
-					:violated_directive,
-					:source_file,
-					:script_sample
-				);
-			")->bind([
-				'blocked_uri' => $report->{'blocked-uri'},
-				'document_uri' => $report->{'document-uri'},
-				'violated_directive' => $report->{'violated-directive'},
-				'source_file' => $report->{'source-file'},
-				'script_sample' => $report->{'script-sample'}
-			]);
-
-			if(!$DB->execute()) {
-				throw new \Exception('Error not successfully recorded', 504);
-			}
-
-			else {
-				http_response_code(202);
-				exit(json_encode($report));
-			}
+			is_null($headers->{'content-length'})
+			or strlen($report) !== (int)$headers->{'content-length'}
+		) {
+			throw new \Exception('Content-Length not set or invalid', 411);
 		}
 
-		else {
-			throw new \Exception('Invalid Content-Type header', 405);
-		}
-	}
+		$report = json_decode($report)->{'csp-report'};
 
-	catch(\Exception $e) {
-		http_response_code($e->getCode());
-		exit(json_encode([
-			'Message' => $e->getMessage(),
-			'Request' => [
-				'Headers' => $headers,
-				'Body' => $report
-			]
-		]));
+		if(! is_object($report) or empty($report)) {
+			throw new \Exception('Invalid csp-report', 400);
+		}
+		if(! isset($report->{'script-sample'})) {
+			$report->{'script-sample'} = '';
+		}
+		if(! isset($report->{'source-file'})) {
+			$report->{'source-file'} = null;
+		}
+
+		$DB = new \shgysk8zer0\Core\PDO('connect.json');
+
+		if(! $DB->connected) {
+			throw new \Exception('Database connection failed', 504);
+		}
+
+		$DB->prepare(
+			"INSERT INTO `CSP_errors` (
+				`blocked-uri`,
+				`document-uri`,
+				`violated-directive`,
+				`source-file`,
+				`script-sample`
+			) VALUES (
+				:blocked_uri,
+				:document_uri,
+				:violated_directive,
+				:source_file,
+				:script_sample
+			);"
+		)->bind([
+			'blocked_uri' => $report->{'blocked-uri'},
+			'document_uri' => $report->{'document-uri'},
+			'violated_directive' => $report->{'violated-directive'},
+			'source_file' => $report->{'source-file'},
+			'script_sample' => $report->{'script-sample'}
+		]);
+
+		if(! $DB->execute()) {
+			throw new \Exception('Error not successfully recorded', 504);
+		} else {
+			http_response_code(202);
+			exit(json_encode($report, JSON_PRETTY_PRINT));
+		}
+	} else {
+		throw new \Exception('Invalid Content-Type header', 405);
 	}
-?>
+} catch(\Exception $e) {
+	http_response_code($e->getCode());
+	exit(json_encode([
+		'Message' => $e->getMessage(),
+		'Request' => [
+			'Headers' => $headers,
+			'Body' => $report
+		]
+	], JSON_PRETTY_PRINT));
+}
