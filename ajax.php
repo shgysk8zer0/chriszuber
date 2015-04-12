7080f1c15a1e2ad7282cd2f7dcbd44832ada8570
Chris Zuber <shgysk8zer0@gmail.com>
2015-04-11 21:10:07 -0700

    Formatting and other fixes
    * Use all `elseif`s on same line as previous condition
    * Exit with JSON_Response class more directly (not `load`, `send`,
    `exit`).

--- ajax.php
+++ ajax.php
@@ -1,45 +1,24 @@
 <?php
-	if(!count($_REQUEST)) {
-		load('handlers/pages');
-	}
+if (! count($_REQUEST)) {
+	load('handlers/pages');
+} elseif (array_key_exists('load_form', $_POST)) {
+	load('handlers/load_form');
+} elseif (array_key_exists('form', $_POST)) {
+	load('handlers/form');
+} elseif (array_key_exists('load_menu', $_POST)) {
+	load('handlers/load_menu');
+} elseif (array_key_exists('datalist', $_REQUEST)) {
+	load('handlers/datalist');
+} elseif (array_key_exists('template', $_REQUEST)) {
+	load('handlers/template');
+} elseif (array_key_exists('action', $_POST)) {
+	load('handlers/action');
+} elseif (array_key_exists('request', $_POST)) {
+	load('handlers/request');
+} elseif (array_key_exists('debug', $_POST)) {
+	load('handlers/debug');
+} elseif (array_key_exists('hangman', $_REQUEST)) {
+	load('handlers/hangman');
+}
 
-	elseif(array_key_exists('load_form', $_POST)) {
-		load('handlers/load_form');
-	}
-
-	elseif(array_key_exists('form', $_POST)) {
-		load('handlers/form');
-	}
-
-	elseif(array_key_exists('load_menu', $_POST)) {
-		load('handlers/load_menu');
-	}
-
-	elseif(array_key_exists('datalist', $_REQUEST)) {
-		load('handlers/datalist');
-	}
-
-	elseif(array_key_exists('template', $_REQUEST)) {
-		load('handlers/template');
-	}
-
-	elseif(array_key_exists('action', $_POST)) {
-		load('handlers/action');
-	}
-
-	elseif(array_key_exists('request', $_POST)) {
-		load('handlers/request');
-	}
-
-	elseif(array_key_exists('debug', $_POST)) { // Debugging only available to admins
-		load('handlers/debug');
-	}
-
-	elseif(array_key_exists('hangman', $_REQUEST)) {
-		load('handlers/hangman');
-	}
-
-	$resp = \shgysk8zer0\Core\json_response::load();
-	$resp->send();
-	exit();
-?>
+exit(\shgysk8zer0\Core\JSON_Response::load());
