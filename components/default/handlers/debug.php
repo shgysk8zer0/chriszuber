1886f53ede79f0e9c05dac3dade4ecd10da5675f
Chris Zuber <shgysk8zer0@gmail.com>
2015-04-11 21:12:04 -0700

    Reformat
    Fix formatting of each `case`

--- components/default/handlers/debug.php
+++ components/default/handlers/debug.php
@@ -4,42 +4,29 @@ check_nonce();
 require_login('admin');
 
 switch(trim($_POST['debug'])) {
+	case 'headers':
+		$resp->info(['HTTP Headers' => headers_list()]);
+		break;
 
-	case 'headers': {
-		$resp->info([
-			'HTTP Headers' => headers_list()
-		]);
-	} break;
+	case 'extensions':
+		$resp->info(['PHP extensions' => get_loaded_extensions()]);
+		break;
 
-	case 'extensions': {
-		$resp->info([
-			'PHP extensions' => get_loaded_extensions()
-		]);
-	} break;
+	case 'modules':
+		$resp->info(['Apache Modules' => apache_get_modules()]);
+		break;
 
-	case 'modules': {
-		$resp->info([
-			'Apache Modules' => apache_get_modules()
-		]);
-	} break;
+	case '_SERVER':
+		$resp->info(['$_SERVER' => $_SERVER]);
+		break;
 
-	case '_SERVER': {
-		$resp->info([
-			'$_SERVER' => $_SERVER
-		]);
-	} break;
+	case '_SESSION':
+		$resp->info(['$_SESSION' => $_SESSION]);
+		break;
 
-	case '_SESSION': {
-		$resp->info([
-			'$_SESSION' => $_SESSION
-		]);
-	} break;
-
-	case '_COOKIES': {
-		$resp->info([
-			'$_COOKIES' => $_COOKIE
-		]);
-	} break;
+	case '_COOKIES':
+		$resp->info(['$_COOKIES' => $_COOKIE]);
+		break;
 }
 $resp->notify(
 	'Debug info sent to console.info',
