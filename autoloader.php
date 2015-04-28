addf4012442f7597d6ad2b9506ee93a48ff00407
Chris Zuber <shgysk8zer0@gmail.com>
2015-04-28 08:34:20 -0700

    Replace functions and autoloader with submodule
    This will (hopefully) eventially make things easier to manage between
    projects.

--- autoloader.php
+++
@@ -1,8 +0,0 @@
-<?php
-set_include_path(join(PATH_SEPARATOR, [
-	realpath(getenv('AUTOLOAD_DIR')),
-	realpath(getenv('CONFIG_DIR')),
-	get_include_path()
-]));
-spl_autoload_extensions(getenv('AUTOLOAD_EXTS'));
-spl_autoload_register(getenv('AUTOLOAD_FUNC'));
