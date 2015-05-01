f258bbf0d6b67208c75d5e51aadef1f2117a9f7e
Chris Zuber <shgysk8zer0@gmail.com>
2015-04-30 23:29:53 -0700

    Update search results
    Modeled after results from DuckDuck Go

--- searchsuggestions.php
+++ searchsuggestions.php
@@ -1,41 +1,41 @@
 <?php
 error_reporting(0);
-$query = array_key_exists('query', $_GET) ? $_GET['query'] : '';
-file_put_contents('search.log', print_r([
-	'query' => $query,
-	'headers' => getallheaders(),
-	'raw' => file_get_contents('php://input'),
-	'time' => date(DATE_W3C)
-], true));
+define('PARAM', 'query');
+if (! array_key_exists(PARAM, $_REQUEST)) {
+	http_response_code(400);
+	exit;
+}
 $PDO = \shgysk8zer0\Core\PDO::load('connect.json');
 if (! $PDO->connected) {
 	http_response_code(500);
 	exit;
 }
 header('Content-Type: application/json');
-exit(json_encode([$query, array_reduce(
-	$PDO('SELECT `keywords` FROM `posts` LIMIT 25'),
-	'reduce_keywords',
-	[]
-)]));
+$matches = $PDO->prepare(
+	'SELECT DISTINCT(`keywords`) as `tags` FROM `posts` WHERE `keywords` LIKE :tags'
+);
+$matches->tags = '%' . str_replace(' ', '%', $_REQUEST[PARAM]) . '%';
+$results = array_reduce(
+	$matches->execute()->getResults(),
+	'reduce_tags',
+	array()
+);
+exit(json_encode($results));
 
 /**
  * Reduce an array of results from PDO query into a unique array
  *
- * @param  array    $all  Carried array of unique results
- * @param  stdClass $tags Single row item
- * @return array          Array of unique results
+ * @param  array    $results  Carried array of unique results
+ * @param  stdClass $tags     Single row item
+ * @return array              Array of unique results
  */
-function reduce_keywords(array $all, \stdClass $tags)
+function reduce_tags(array $results = array(), \stdClass $post)
 {
-	return array_merge(
-		array_filter(
-			explode(',', $tags->keywords),
-			function($tag) use ($all)
-			{
-				return ! in_array($tag, $all);
-			}
-		),
-		$all
-	);
+	foreach (explode(',', $post->tags) as $tag) {
+		$tag = trim(strtolower($tag));
+		if (! in_array(['phrase' => $tag], $results) and preg_match('/^' . preg_quote($tag) .'/i', $_REQUEST[PARAM])) {
+			array_push($results, ['phrase' => $tag]);
+		}
+	}
+	return $results;
 }
