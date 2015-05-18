543cfa96bfc3da6492ea8863492156cb240a7c2e
Chris Zuber <shgysk8zer0@gmail.com>
2015-05-18 07:29:34 -0700

    Prefetch combined.svg
    Add a "prefetch" link to load combined icons file.

--- components/default/head.php
+++ components/default/head.php
@@ -67,6 +67,7 @@
 <link rel="icon" type="image/svg" sizes="any" href="favicon.svgz?t=<?=time()?>"/>
 <link rel="alternate icon" type="image/png" sizes="16x16" href="favicon.png"/>
 <link rel="search" type="application/opensearchdescription+xml" title="<?=TITLE?> Tag Search" href="<?=URL?>opensearch.php"/>
+<link rel="prefetch" href="images/icons/combined.svg" type="image/svg+xml"/>
 <?php if(localhost() and BROWSER === 'Firefox'):?>
 	<link rel="stylesheet" type="text/css" href="stylesheets/<?=THEME?>/import.css" media="all"/>
 <?php else:?>
