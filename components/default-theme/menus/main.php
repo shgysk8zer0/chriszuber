396d103663e4419feb47be743c5c780246229e4c
Chris Zuber <shgysk8zer0@gmail.com>
2015-05-25 19:51:11 -0700

    Added navigation to contextmenu
    Added navigation to top, bottom, & comments

--- components/default-theme/menus/main.php
+++ components/default-theme/menus/main.php
@@ -2,6 +2,9 @@
 	$posts = get_recent_posts(10);
 ?>
 <menu type="context" id="main_menu">
+	<menuitem label="Scroll to top" icon="images/octicons/svg/jump-up.svg" data-scroll-to="body > header"></menuitem>
+	<menuitem label="Scroll to bottom" icon="images/octicons/svg/jump-down.svg" data-scroll-to="body > footer"></menuitem>
+	<menuitem label="Go to comments section" icon="images/octicons/svg/comment.svg" data-scroll-to="#comments_section"></menuitem>
 	<menu label="Posts">
 		<menuitem label="Home" icon="images/icons/home.svgz" data-link="<?=URL?>" data-cache="home"></menuitem>
 		<?php foreach($posts as $post):?>
