2fccf649af2d053d399ab7bd619e624e47b1b416
Chris Zuber <shgysk8zer0@gmail.com>
2015-06-24 18:16:06 -0700

    Enhancements to view_source (now similar to autoloader)
    * File converted to lower case (since SPL is used, this must be the
    case)
    * Now used AUTOLOAD_DIR for path
    * Automatically use ".php" as extension when not given
    * Use new Dialog class for displaying source code

--- components/default-theme/handlers/view_source.php
+++ components/default-theme/handlers/view_source.php
@@ -1,27 +1,20 @@
 <?php
 $resp = \shgysk8zer0\Core\JSON_Response::load();
 try {
-	$file = strtolower(join(
-		DIRECTORY_SEPARATOR,
-		[
-			BASE,
-			'classes',
-			str_replace('\\', DIRECTORY_SEPARATOR, trim($_REQUEST['view_source'], '\\'))
-		]
-	));
+	$file = realpath(getenv('AUTOLOAD_DIR')) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, strtolower($_REQUEST['view_source']));
+
+	if (empty(pathinfo($file, PATHINFO_EXTENSION))) {
+		$file .= '.php';
+	}
 	if (! file_exists($file)) {
 		throw new \Exception("{$_REQUEST['view_source']} not found", 200);
 	}
-	$dialog = new \shgysk8zer0\Core\HTML_El('dialog', null, null, true);
-	$dialog->{'@id'} = 'source_viewer_dialog';
-	$dialog->button = ['@data-delete' => '#source_viewer_dialog'];
-	$dialog->br = null;
-	$dialog(\shgysk8zer0\Core\File::load($file)->highlightFile());
+	$dialog = new \shgysk8zer0\Core\Elements\Dialog(
+		'source_viewer',
+		\shgysk8zer0\Core\File::load($file)->highlightFile()
+	);
 
-	$resp->append(
-		'article',
-		$dialog
-	)->showModal('#source_viewer_dialog');
+	$resp->append('article', $dialog)->showModal($dialog->id);
 } catch(\Exception $e) {
 	http_response_code($e->getCode());
 	$resp->notify(
