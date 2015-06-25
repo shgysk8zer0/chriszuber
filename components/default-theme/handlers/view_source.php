ae055aa5219ff82ef287666538c717ec4ccb42bd
Chris Zuber <shgysk8zer0@gmail.com>
2015-06-24 19:56:08 -0700

    Update to use `highlight_file`
    Use built-in function instead of class & method

--- components/default-theme/handlers/view_source.php
+++ components/default-theme/handlers/view_source.php
@@ -1,18 +1,15 @@
 <?php
 $resp = \shgysk8zer0\Core\JSON_Response::load();
 try {
-	$file = realpath(getenv('AUTOLOAD_DIR')) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, strtolower($_REQUEST['view_source']));
+	$filename = realpath(getenv('AUTOLOAD_DIR')) . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, strtolower($_REQUEST['view_source']));
 
-	if (empty(pathinfo($file, PATHINFO_EXTENSION))) {
-		$file .= '.php';
+	if (empty(pathinfo($filename, PATHINFO_EXTENSION))) {
+		$filename .= '.php';
 	}
-	if (! file_exists($file)) {
+	if (! file_exists($filename)) {
 		throw new \Exception("{$_REQUEST['view_source']} not found", 200);
 	}
-	$dialog = new \shgysk8zer0\Core\Elements\Dialog(
-		'source_viewer',
-		\shgysk8zer0\Core\File::load($file)->highlightFile()
-	);
+	$dialog = new \shgysk8zer0\Core\Elements\Dialog('source_viewer', highlight_file($filename, true));
 
 	$resp->append('article', $dialog)->showModal($dialog->id);
 } catch(\Exception $e) {
