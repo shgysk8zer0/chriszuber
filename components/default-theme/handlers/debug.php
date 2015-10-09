433b54647c0bee5e9bd3f41b19096ecefb24c3ed
Chris Zuber <shgysk8zer0@gmail.com>
2015-10-08 22:38:41 -0700

    Use Console class instead of JSON_Response
    Currently doesn't work well in Firefox due to issues converting objects
    to strings, but I'm doing this anyways for future compatibility sake.

--- components/default-theme/handlers/debug.php
+++ components/default-theme/handlers/debug.php
@@ -4,35 +4,35 @@ require_login('admin');
 
 switch($_POST['debug']) {
 	case 'headers':
-		$resp->dir(['HTTP Headers' => headers_list()]);
+		$console->info(['HTTP Headers' => headers_list()]);
 		break;
 
 	case 'extensions':
-		$resp->dir(['PHP extensions' => get_loaded_extensions()]);
+		$console->info(['PHP extensions' => get_loaded_extensions()]);
 		break;
 
 	case 'modules':
-		$resp->dir(['Apache Modules' => apache_get_modules()]);
+		$console->info(['Apache Modules' => apache_get_modules()]);
 		break;
 
 	case '_SERVER':
-		$resp->dir(['$_SERVER' => $_SERVER]);
+		$console->info(['$_SERVER' => $_SERVER]);
 		break;
 
 	case '_SESSION':
-		$resp->dir(['$_SESSION' => $_SESSION]);
+		$console->info(['$_SESSION' => $_SESSION]);
 		break;
 
 	case '_COOKIES':
-		$resp->dir(['$_COOKIES' => $_COOKIE]);
+		$console->info(['$_COOKIES' => $_COOKIE]);
 		break;
 
 	case 'vars':
-		$resp->dir(['Defined Vars' => get_defined_vars()]);
+		$console->info(['Defined Vars' => get_defined_vars()]);
 		break;
 
 	default:
-		$resp->error(sprintf('Unhandled debug request: %s', $_POST['debug']));
+		$console->error(sprintf('Unhandled debug request: %s', $_POST['debug']));
 }
 $resp->notify(
 	'Debug info sent to console',
